50e0615d36ce1f4c944b9be8c81bb09a690976f2
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2011-05-05 15:25:35 +0200

    orienter is finished - untested though because I first need to deal with multi-segment meshes..

--- viennamesh/adaptors/orienter.hpp
+++ viennamesh/adaptors/orienter.hpp
@@ -18,6 +18,7 @@
 // *** vienna includes
 #include "viennamesh/adaptors/base.hpp"
 #include "viennagrid/domain.hpp"
+#include "viennadata/api.hpp"
 
 // *** boost includes
 #include <boost/array.hpp>
@@ -77,11 +78,16 @@ struct mesh_adaptor <viennamesh::tag::orienter>
    typedef domain_type::segment_type                                                                  SegmentType;
    typedef viennagrid::result_of::point_type<DomainConfiguration>::type                               PointType;
    typedef viennagrid::result_of::ncell_type<DomainConfiguration, 0>::type                            VertexType;
+   typedef viennagrid::result_of::ncell_type<DomainConfiguration, 1>::type                            EdgeType;   
    typedef viennagrid::result_of::ncell_type<DomainConfiguration, CellTag::topology_level>::type      CellType;
-   typedef viennagrid::result_of::ncell_container<SegmentType, CellTag::topology_level>::type   CellContainer;      
+   typedef viennagrid::result_of::ncell_container<SegmentType, CellTag::topology_level>::type         CellContainer;      
    typedef viennagrid::result_of::iterator<CellContainer>::type                                       CellIterator;      
-   typedef viennagrid::result_of::ncell_container<CellType, 0>::type                            VertexOnCellContainer;
+   typedef viennagrid::result_of::ncell_container<CellType, 0>::type                                  VertexOnCellContainer;
    typedef viennagrid::result_of::iterator<VertexOnCellContainer>::type                               VertexOnCellIterator;      
+   typedef viennagrid::result_of::ncell_container<CellType, 1>::type                                  EdgeOnCellContainer;
+   typedef viennagrid::result_of::iterator<EdgeOnCellContainer>::type                                 EdgeOnCellIterator;      
+   typedef viennagrid::result_of::ncell_container<EdgeType, CellTag::topology_level>::type            CellOnEdgeContainer;
+   typedef viennagrid::result_of::iterator<CellOnEdgeContainer>::type                                 CellOnEdgeIterator;   
    
    static const int DIMG = DomainConfiguration::dimension_tag::value;
    static const int DIMT = DomainConfiguration::cell_tag::topology_level;   
@@ -108,6 +114,11 @@ struct mesh_adaptor <viennamesh::tag::orienter>
    #ifdef MESH_ADAPTOR_DEBUG
       std::cout << "## MeshAdaptor::"+id+" - starting up .." << std::endl;
    #endif            
+   
+      std::size_t global_consistant = 0;
+      std::size_t global_leftovers = 0;
+      std::size_t global_errors = 0;   
+      std::size_t norm_vector_corrections = 0;
    
       // traverse the segments ..
       //
@@ -219,23 +230,225 @@ struct mesh_adaptor <viennamesh::tag::orienter>
                temp_cell[vi++] = vocit->getID();
             }            
 
-            // return the orientation by using reversed indices of the original cell
+            // return the orientation by using reversed indices 
+            // of the original cell
+            //
             vertices[0] = &(domain->vertex(temp_cell[2]));               
             vertices[1] = &(domain->vertex(temp_cell[1]));
             vertices[2] = &(domain->vertex(temp_cell[0]));
 
             seed_cell.setVertices(vertices);
             seed_cell.fill(*domain);
-         }
+            
+            norm_vector_corrections++;
+         } 
          
-         // now that the seed cell is oriented correctly, 
-         // spread the word to all the other cells of the segment
+         // initiate an orientation quantity on all cells
          //
+         for (CellIterator cit = cells.begin(); cit != cells.end(); ++cit)
+         {
+           viennadata::access<std::string, int>("orient")(*cit) = 0; 
+         }
+         // the first cell is already oriented
+         //
+         viennadata::access<std::string, int>("orient")(seed_cell) = 1; 
+
+         cell_orienter_recursive(domain, seed_cell);
+
+         std::size_t consistant = 0;
+         std::size_t leftovers = 0;
+         std::size_t errors = 0;
+         for (CellIterator cit = cells.begin(); cit != cells.end(); ++cit)
+         {
+            if(viennadata::access<std::string, int>("orient")(*cit) == 0) 
+               leftovers++;
+            else
+            if(viennadata::access<std::string, int>("orient")(*cit) == 1) 
+               consistant++;
+            else
+            if(viennadata::access<std::string, int>("orient")(*cit) == -1) 
+               errors++;
+         }      
+
+         global_consistant += consistant;
+         global_leftovers  += leftovers;
+         global_errors     += errors;
+
+      #ifdef MESH_ADAPTOR_DEBUG
+         std::cout << "  finished segment - statistics:" << std::endl;
+         std::cout << "      consistants:  " << consistant << std::endl;
+         std::cout << "      leftovers:    " << leftovers << std::endl;
+         std::cout << "      errors:       " << errors << std::endl;                  
+      #endif                              
+         if(errors != 0 || leftovers != 0)
+            std::cerr << "## MeshAdaptor::"+id+" - segment orientation experienced errors" << std::endl;
+            
+         // remove the orientation quantity of the cells of the current segment
+         //
+         for (CellIterator cit = cells.begin(); cit != cells.end(); ++cit)
+         {
+           viennadata::erase<std::string, int>("orient")(*cit); 
+         }            
       }
-   
+   #ifdef MESH_ADAPTOR_DEBUG
+      std::cout << std::endl;
+      std::cout << "------------------------------------------------------ " << std::endl;
+   #endif
+      if(global_errors != 0 || global_leftovers != 0)
+         std::cerr << "## MeshAdaptor::"+id+" - ERROR: segment orientation failed" << std::endl;         
+   #ifdef MESH_ADAPTOR_DEBUG
+      else std::cout << "## MeshAdaptor::"+id+" - finished successfully!" << std::endl;
+         
+      std::cout << "------------------------------------------------------ " << std::endl;
+      std::cout << "## MeshAdaptor::"+id+" - result statistics:" << std::endl;
+      std::cout << "   consistants:      " << global_consistant << std::endl;
+      std::cout << "   seed-corrections: " <<  norm_vector_corrections << std::endl;
+      std::cout << "   leftovers:        " << global_leftovers << std::endl;
+      std::cout << "   errors:           " << global_errors << std::endl;                  
+      std::cout << "------------------------------------------------------ " << std::endl;      
+   #endif                                    
       return domain;
    }
    // -------------------------------------------------------------------------------------
+   
+   // -------------------------------------------------------------------------------------
+   template<typename DomainTPtr, typename CellT>
+   bool cell_orienter_recursive(DomainTPtr& domain, CellT& cell)
+   {
+      // investigate how many neighbour cells have been dealt with so far ..
+      //
+      long number_of_not_oriented_cells = 0;
+
+      EdgeOnCellContainer edges = viennagrid::ncells<1>(cell);
+
+      for (EdgeOnCellIterator eocit = edges.begin(); eocit != edges.end(); ++eocit)
+      {
+         CellOnEdgeContainer cells = viennagrid::ncells<CellTag::topology_level>(*eocit, *domain);
+
+         for (CellOnEdgeIterator coeit = cells.begin(); coeit != cells.end(); ++coeit)
+         {         
+            if(viennadata::access<std::string, int>("orient")(*coeit) == 0)
+               number_of_not_oriented_cells++;
+         }
+      }                  
+      // stop condition for recursion
+      //
+      if (number_of_not_oriented_cells == 0) return true;      
+      
+      // orient the adjacent cells according to the given cell
+      //
+      for (EdgeOnCellIterator eocit = edges.begin(); eocit != edges.end(); ++eocit)
+      {
+         CellOnEdgeContainer cells = viennagrid::ncells<CellTag::topology_level>(*eocit, *domain);
+
+         for (CellOnEdgeIterator coeit = cells.begin(); coeit != cells.end(); ++coeit)
+         {         
+            // if this cell is the reference cell, skip ..
+            //
+            if( coeit->getID() == cell.getID() )   continue;
+            
+            // if this cell has already been dealt with regarding orientation, skip ..
+            // 
+            if (viennadata::access<std::string, int>("orient")(*coeit) != 0)     continue;            
+            
+            if(is_consistant_orientation(*coeit, cell))
+            { 
+               viennadata::access<std::string, int>("orient")(*coeit) = 1;
+            }
+            // repair the orientation
+            else
+            {
+               boost::array<index_type, CELLSIZE>    temp_cell;
+               std::size_t vi = 0;
+               VertexOnCellContainer vertices_for_cell = viennagrid::ncells<0>(*coeit);
+               for (VertexOnCellIterator vocit = vertices_for_cell.begin();
+                   vocit != vertices_for_cell.end();
+                   ++vocit)
+               {
+                  temp_cell[vi++] = vocit->getID();
+               }                        
+            
+               VertexType *vertices[viennagrid::subcell_traits<CellTag, 0>::num_elements];  
+               vertices[0] = &(domain->vertex(temp_cell[2]));               
+               vertices[1] = &(domain->vertex(temp_cell[1]));
+               vertices[2] = &(domain->vertex(temp_cell[0]));
+               coeit->setVertices(vertices);
+               coeit->fill(*domain);               
+               
+               // check again to verify ..
+               //
+               if(is_consistant_orientation(*coeit, cell))
+               { 
+                  viennadata::access<std::string, int>("orient")(*coeit) = 1;
+               }               
+               else
+               {
+                  viennadata::access<std::string, int>("orient")(*coeit) = -1;
+                  std::cerr << "ERROR @ cell-orienter-recursive: could not repair orientation .." << std::endl;
+               }
+            }
+         }
+      }                        
+      
+      // by now, all adjacent cells have been dealt with
+      // step into next recursion level
+      //
+      for (EdgeOnCellIterator eocit = edges.begin(); eocit != edges.end(); ++eocit)
+      {
+         CellOnEdgeContainer cells = viennagrid::ncells<CellTag::topology_level>(*eocit, *domain);
+
+         for (CellOnEdgeIterator coeit = cells.begin(); coeit != cells.end(); ++coeit)
+         {         
+            if( coeit->getID() == cell.getID() )   continue;
+            cell_orienter_recursive(domain, *coeit);            
+         }
+      }                  
+      return true; // TODO is this right?
+   }
+   // -------------------------------------------------------------------------------------      
+   
+   // -------------------------------------------------------------------------------------         
+   template<typename CellT>
+   bool is_consistant_orientation(CellT & c1, CellT & c2)
+   {
+      // transfer input viennagrid cells to boost arrays
+      // to support a more convienient vertex access level
+      //
+      boost::array<index_type, CELLSIZE>  bc1, bc2;
+      std::size_t k = 0;
+      VertexOnCellContainer vertices_c1 = viennagrid::ncells<0>(c1);
+      for (VertexOnCellIterator vocit = vertices_c1.begin();
+          vocit != vertices_c1.end();
+          ++vocit)
+      {
+         bc1[k++] = vocit->getID();
+      }               
+      k = 0;
+      VertexOnCellContainer vertices_c2 = viennagrid::ncells<0>(c2);
+      for (VertexOnCellIterator vocit = vertices_c2.begin();
+          vocit != vertices_c2.end();
+          ++vocit)
+      {
+         bc2[k++] = vocit->getID();
+      }               
+
+      // checks, if two out of n points are oriented consistently
+      //   
+      std::size_t i, j;
+      for(i = 0; i <= 2; i++)
+      {
+         for(j = 0; j <= 2; j++)
+         {
+            if (bc1[(i+1)%3]  == bc2[j] &&    
+                bc1[i]        == bc2[(j+1)%3])
+            {
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+   // -------------------------------------------------------------------------------------      
    
    std::string id;
 };
