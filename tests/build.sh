2fbcf21f0c58b5e08f3b27f627f538230d3f1548
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2011-05-24 15:13:25 +0200

    added hull adaptor test which tests a set of different bnd files

--- tests/build.sh
+++ tests/build.sh
@@ -5,13 +5,11 @@ CURRENT=$PWD
 CORES=`grep -c ^processor /proc/cpuinfo`
 echo "cores: " $CORES
 
-#CORES=1
-
 mkdir -p build/
 cd build/
-cmake -D CMAKE_BUILD_TYPE=Debug\
-      -D CGAL_INC_DIR=/mnt/data/libraries/cgal/cgal-install/include\
-      -D CGAL_LIB_DIR=/mnt/data/libraries/cgal/cgal-install/lib\
+cmake -D CMAKE_BUILD_TYPE=Release\
+      -D CGAL_INC_DIR=$CGALINC\
+      -D CGAL_LIB_DIR=$CGALLIB\
       ../
 make -j$CORES
 cd ..
---
+++ tests/src/meta_selection.cpp
@@ -0,0 +1,23 @@
+/* =============================================================================
+   Copyright (c) 2011, Institute for Microelectronics, TU Wien
+   http://www.iue.tuwien.ac.at
+                             -----------------
+                 ViennaMesh - The Vienna Mesh Generation Framework
+                             -----------------
+
+   authors:    Josef Weinbub                         weinbub@iue.tuwien.ac.at
+
+
+   license:    see file LICENSE in the base directory
+============================================================================= */
+
+#include <iostream>
+
+
+int main(int argc, char * argv[])
+{
+
+    
+
+   return 0;
+}
