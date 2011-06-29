8a4f9e1fef0840050a0995cc84b5c47273dee10f
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2011-06-29 16:29:05 +0200

    minor updates ..

--- src/vmesh.cpp
+++ src/vmesh.cpp
@@ -28,21 +28,6 @@
 template<typename WrappedDatastructureT>
 void process(WrappedDatastructureT& data)
 {
-   typedef viennamesh::result_of::mesh_generator<viennamesh::tag::triangle>::type        triangle_mesh_generator_type;
-   triangle_mesh_generator_type     triangle_mesher;      
-
-
-
-
-
-
-
-
-
-
-
-
-
    // prepare a hull mesher
    //   generates a 2-simplex unstructured mesh embedded in a three-dimensional
    //   geometrical space
