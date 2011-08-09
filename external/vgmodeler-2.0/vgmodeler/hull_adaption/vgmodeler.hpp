79c8fd9dd5519d8188476a0c45fb92153ff6475d
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2011-08-09 11:16:08 +0200

    stabilized vgmodeler::datastructure loader

--- external/vgmodeler-2.0/vgmodeler/hull_adaption/vgmodeler.hpp
+++ external/vgmodeler-2.0/vgmodeler/hull_adaption/vgmodeler.hpp
@@ -23,6 +23,7 @@
 #include <iostream>   
    
    
+  
 // *** include vgmodelers own netgen
 // note: required gsse01 to be included before including 
 // the nglib.h
@@ -35,6 +36,44 @@ namespace vgmnetgen {
 
 
 namespace vgmodeler {
+
+template<typename VectorT>
+VectorT 
+cross_prod(VectorT const& v1, VectorT const& v2)
+{
+   VectorT result;
+   result[0] = v1[1]*v2[2]-v1[2]*v2[1];
+   result[1] = v1[2]*v2[0]-v1[0]*v2[2];
+   result[2] = v1[0]*v2[1]-v1[1]*v2[0];
+   return result;
+}
+
+template<typename VectorT>
+typename VectorT::value_type 
+in_prod(VectorT const& v1, VectorT const& v2)
+{
+   return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
+
+template<typename VectorT>
+typename VectorT::value_type 
+norm_2(VectorT const& v)
+{
+   typename VectorT::value_type result(0);
+   for(std::size_t i = 0; i < v.size(); i++)
+      result += v[i]*v[i];
+   return std::sqrt(result);   
+}
+
+template<typename VectorT>
+typename VectorT::value_type 
+norm_2_squared(VectorT const& v)
+{
+   typename VectorT::value_type result(0);
+   for(std::size_t i = 0; i < v.size(); i++)
+      result += v[i]*v[i];
+   return result;   
+}
 
 struct hull_adaptor
 {
@@ -50,7 +89,7 @@ struct hull_adaptor
       
       mp.maxh              = 100000;       // Maximum global mesh size allowed. 
       mp.fineness          = 0.5;          // Mesh density: 0...1 (0 => coarse; 1 => fine). 
-      mp.secondorder       = 2;            // Generate second-order surface and volume elements. 
+      mp.secondorder       = 0;            // Generate second-order surface and volume elements. 
       mp.meshsize_filename = cfilename;    // Optional external mesh size file. 
       mp.quad_dominated    = 0;            // Creates a Quad-dominated mesh. 
 
@@ -64,41 +103,18 @@ struct hull_adaptor
       vgmnetgen::Ng_Exit();
    }
 
-//   // add hull element to data structure - only triangle geometry informaiton 
-//   // available --> compute the normal vector
-//   // 
-//   template<typename TriangleT>
-//   void add_hull_element(TriangleT const& tri)
-//   {
-//      double p1[3];
-//      double p2[3];
-//      double p3[3];
-//      double n[3];   
-
-//      p1[0] = tri[0][0];
-//      p1[1] = tri[0][1];
-//      p1[2] = tri[0][2];            
-
-//      p2[0] = tri[1][0];
-//      p2[1] = tri[1][1];
-//      p2[2] = tri[1][2];            
-
-//      p3[0] = tri[2][0];
-//      p3[1] = tri[2][1];
-//      p3[2] = tri[2][2];            
-
-//      n[0] = tri_normal[0];
-//      n[1] = tri_normal[1];
-//      n[2] = tri_normal[2];
-
-////      std::cout << "adding triangle:" << std::endl;
-////      std::cout << "  p1: " << p1[0] << " " << p1[1] << " " << p1[2] << std::endl;
-////      std::cout << "  p2: " << p2[0] << " " << p2[1] << " " << p2[2] << std::endl;
-////      std::cout << "  p3: " << p3[0] << " " << p3[1] << " " << p3[2] << std::endl;
-////      std::cout << "  n:  " << n[0] << " " << n[1] << " " << n[2] << std::endl;
-
-//      vgmnetgen::Ng_STL_AddTriangle(geom, p1, p2, p3, n);
-//   }
+   // add hull element to data structure - only triangle geometry informaiton 
+   // available --> compute the normal vector
+   // 
+   template<typename TriangleT>
+   void add_hull_element(TriangleT const& tri)
+   {
+      typedef typename TriangleT::value_type  vector_type;
+      vector_type normal = vgmodeler::cross_prod(tri[1]-tri[0], tri[2]-tri[0]);
+      normal /= vgmodeler::norm_2(normal);
+      
+      add_hull_element(tri, normal);
+   }
 
    // add hull element to data structure - the normal vector information is available
    // 
@@ -122,9 +138,41 @@ struct hull_adaptor
       p3[1] = tri[2][1];
       p3[2] = tri[2][2];            
 
-      n[0] = tri_normal[0];
-      n[1] = tri_normal[1];
-      n[2] = tri_normal[2];
+      // the following checks are suggested in Netgen::STLTopology::Load - function
+      //
+      if(vgmodeler::norm_2(tri_normal) <= 1.0E-5)
+      {
+         VectorT temp = vgmodeler::cross_prod(tri[1]-tri[0], tri[2]-tri[0]);
+         temp /= vgmodeler::norm_2(temp);
+         n[0] = temp[0];
+         n[1] = temp[1];
+         n[2] = temp[2];                  
+      }
+      else
+      {
+         VectorT hnormals = vgmodeler::cross_prod(tri[1]-tri[0], tri[2]-tri[0]);
+         hnormals /= vgmodeler::norm_2(hnormals);      
+      
+         if( vgmodeler::in_prod(tri_normal, hnormals) < 0.5 )
+         {
+            std::cerr << "VGModeler::add_hull_element: detected bad normal .. " << std::endl;
+         }
+      
+         n[0] = tri_normal[0];
+         n[1] = tri_normal[1];
+         n[2] = tri_normal[2];
+      }
+
+      // final numerical/robustness checks
+      
+      double ref = 1.0E-16;
+      if( !(vgmodeler::norm_2_squared(tri[0]-tri[1]) > ref) && 
+          !(vgmodeler::norm_2_squared(tri[0]-tri[2]) > ref) && 
+          !(vgmodeler::norm_2_squared(tri[1]-tri[2]) > ref) )
+      {
+         std::cerr << "VGModeler::add_hull_element: detected very small triangle .. " << std::endl;
+      }
+
 
 //      std::cout << "adding triangle:" << std::endl;
 //      std::cout << "  p1: " << p1[0] << " " << p1[1] << " " << p1[2] << std::endl;
@@ -133,6 +181,8 @@ struct hull_adaptor
 //      std::cout << "  n:  " << n[0] << " " << n[1] << " " << n[2] << std::endl;
 
       vgmnetgen::Ng_STL_AddTriangle(geom, p1, p2, p3, n);
+
+      
    }
 
    
