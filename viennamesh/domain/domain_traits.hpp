82f4327f71b29c273f3c9399e0b4d65c8448eab2
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-04-04 17:40:36 +0200

    traits for domains

---
+++ viennamesh/domain/domain_traits.hpp
@@ -0,0 +1,92 @@
+#ifndef VIENNAMESH_DOMAIN_TRAITS_HPP
+#define VIENNAMESH_DOMAIN_TRAITS_HPP
+
+#include "viennagrid/domain/config.hpp"
+#include <boost/concept_check.hpp>
+
+
+namespace viennamesh
+{
+
+    namespace domain_traits
+    {
+        
+        template<typename domain_type>
+        struct is_vienna_grid_topologic
+        {
+            static const bool value = false;
+        };
+        
+        template<typename container_collection_type_, typename inserter_type_>
+        struct is_vienna_grid_topologic< viennagrid::topologic_domain_t<container_collection_type_,inserter_type_> >
+        {
+            static const bool value = true;
+        };
+        
+        
+        
+        template<typename domain_type>
+        struct is_vienna_grid_geometric
+        {
+            static const bool value = false;
+        };
+        
+        template<typename vector_type_, typename topologic_domain_type_, typename metainfo_collection_type_>
+        struct is_vienna_grid_geometric< viennagrid::geometric_domain_t<vector_type_,topologic_domain_type_, metainfo_collection_type_> >
+        {
+            static const bool value = true;
+        };
+        
+        
+        
+        template<typename domain_type>
+        struct is_vienna_grid_geometric
+        {
+            static const bool value = is_vienna_grid_topologic<domain_type>::value || is_vienna_grid_geometric<domain_type>::value;
+        };
+        
+        
+        
+        template<typename point_type>
+        struct geometric_dimension
+        {
+            static const unsigned int value = point_type::dim;
+        };
+        
+        template<typename vector_type_, typename topologic_domain_type_, typename metainfo_collection_type_>
+        struct geometric_dimension< viennagrid::geometric_domain_t<vector_type_,topologic_domain_type_, metainfo_collection_type_> >
+        {
+            static const unsigned int value = geometric_dimension<vector_type_>::value;
+        };
+        
+        
+        
+        template<typename domain_type, typename tag>
+        struct has_element
+        {
+            static const bool value = false;
+        };
+        
+        template<typename container_collection_type_, typename inserter_type_, typename tag>
+        struct has_element< viennagrid::topologic_domain_t<container_collection_type_,inserter_type_> >
+        {
+            static const bool value = !viennameta::_equal<
+                    viennameta::typemap::result_of::find<typename container_collection_type_::typemap, tag>,
+                    viennameta::not_found
+                >::value;
+        };
+        
+        template<typename vector_type_, typename topologic_domain_type_, typename metainfo_collection_type_, typename tag>
+        struct has_element< viennagrid::geometric_domain_t<vector_type_,topologic_domain_type_, metainfo_collection_type_> >
+        {
+            static const bool value = !viennameta::_equal<
+                    viennameta::typemap::result_of::find<typename topologic_domain_type_::container_collection_type::typemap, tag>,
+                    viennameta::not_found
+                >::value;
+        };
+        
+    }
+    
+}
+
+#endif
