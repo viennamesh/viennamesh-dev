ec545081d212b6ed850282691555e1119ac87aa3
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-04-22 16:18:30 +0200

    added segment information support

--- viennamesh/base/convert.hpp
+++ viennamesh/base/convert.hpp
@@ -1,6 +1,8 @@
 #ifndef VIENNAMESH_BASE_CONVERT_HPP
 #define VIENNAMESH_BASE_CONVERT_HPP
 
+#include "viennamesh/base/segments.hpp"
+
 namespace viennamesh
 {
     template<typename input_domain_type, typename output_domain_type>
@@ -12,7 +14,11 @@ namespace viennamesh
     template<typename input_domain_type, typename output_domain_type>
     bool convert( input_domain_type const & in, output_domain_type & out )
     {
-        return convert_impl<input_domain_type, output_domain_type>::convert(in, out);
+        bool status = convert_impl<input_domain_type, output_domain_type>::convert(in, out);;
+        
+        copy_segment_information(in, out);
+        
+        return status;
     }
     
     template<typename domain_type>
