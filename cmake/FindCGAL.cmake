1b3f34f7ea347ed666ccf00e34d9bc825a7be12b
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-11-11 12:17:36 +0100

    Better use of CGAL cmake files (CGAL_USE_FILE cannot be used properly because it overrides previous defined cmake flags)

--- cmake/FindCGAL.cmake
+++ cmake/FindCGAL.cmake
@@ -13,27 +13,27 @@
 # Construct consitent error messages for use below.
 set(CGAL_DIR_DESCRIPTION "directory containing CGALConfig.cmake. This is either the binary directory where CGAL was configured or PREFIX/lib/CGAL for an installation.")
 set(CGAL_DIR_MESSAGE     "CGAL not found.  Set the CGAL_DIR cmake variable or environment variable to the ${CGAL_DIR_DESCRIPTION}")
- 
+
 set(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS true)
- 
+
 if ( NOT CGAL_DIR )
-  
+
   # Get the system search path as a list.
   if(UNIX)
     string(REGEX MATCHALL "[^:]+" CGAL_DIR_SEARCH1 "$ENV{PATH}")
   else()
     string(REGEX REPLACE "\\\\" "/" CGAL_DIR_SEARCH1 "$ENV{PATH}")
   endif()
-  
+
   string(REGEX REPLACE "/;" ";" CGAL_DIR_SEARCH2 "${CGAL_DIR_SEARCH1}")
 
   # Construct a set of paths relative to the system search path.
   set(CGAL_DIR_SEARCH "")
-  
+
   foreach(dir ${CGAL_DIR_SEARCH2})
-  
+
     set(CGAL_DIR_SEARCH ${CGAL_DIR_SEARCH} ${dir}/../lib/CGAL )
-      
+
   endforeach()
 
 
@@ -68,11 +68,11 @@ if ( NOT CGAL_DIR )
     # Help the user find it if we cannot.
     DOC "The ${CGAL_DIR_DESCRIPTION}"
   )
-  
+
 endif()
 
 if ( CGAL_DIR )
-  
+
   if ( EXISTS "${CGAL_DIR}/CGALConfig.cmake" )
     include( "${CGAL_DIR}/CGALConfig.cmake" )
     set( CGAL_FOUND TRUE )
