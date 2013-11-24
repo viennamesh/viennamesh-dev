b8167f35063fec45c84ecec9c0c569478b74d2f3
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-11-24 11:29:05 +0100

    Added new example: seed point meshing with building your own geometry and 3 segments

---
+++ examples/tutorial/tetgen_seedpoints_3segments.cpp
@@ -0,0 +1,204 @@
+#include "viennamesh/algorithm/tetgen.hpp"
+#include "viennamesh/algorithm/seed_point_locator.hpp"
+#include "viennamesh/algorithm/file_reader.hpp"
+#include "viennamesh/algorithm/file_writer.hpp"
+
+
+
+// This method helps us to create or fetch a line based on vertex handles
+// if a line is already present, we just use that line, otherwise we create it
+// We are using a std::map< std::pair<VertexID, VertexID>, LineHandle > for keeping track of all lines in the mesh
+template<typename MeshT, typename VertexHandleT, typename VertexLineMapT>
+typename viennagrid::result_of::line_handle<MeshT>::type get_make_line(MeshT & mesh, VertexLineMapT & vertex_line_map, VertexHandleT const & vtxh0, VertexHandleT const & vtxh1)
+{
+  // querying the ID of the vertices
+  typedef typename viennagrid::result_of::vertex_id<MeshT>::type VertexIDType;
+  VertexIDType id0 = viennagrid::dereference_handle(mesh, vtxh0).id();
+  VertexIDType id1 = viennagrid::dereference_handle(mesh, vtxh1).id();
+
+  // creating the key for the line, note that we order the IDs within the pair for uniqueness
+  std::pair<VertexIDType, VertexIDType> key = std::make_pair( std::min(id0, id1), std::max(id0, id1) );
+
+  // searching for the line in our map
+  typename VertexLineMapT::iterator it = vertex_line_map.find(key);
+  if (it != vertex_line_map.end())
+  {
+    // found -> return
+    return it->second;
+  }
+  else
+  {
+    // not found -> create the line
+    typename viennagrid::result_of::line_handle<MeshT>::type tmp = viennagrid::make_line( mesh, vtxh0, vtxh1 );
+    vertex_line_map[ key ] = tmp;
+    return tmp;
+  }
+}
+
+
+// This method helps us creating a quad PLC based on 4 vertex handles
+// we use get_make_line (see above) and a vertex IDs to line map for keeping track of all lines
+template<typename MeshT, typename VertexLineMapT, typename VertexHandleT>
+typename viennagrid::result_of::plc_handle<MeshT>::type make_quad_plc(MeshT & mesh, VertexLineMapT & vertex_line_map, VertexHandleT v0, VertexHandleT v1, VertexHandleT v2, VertexHandleT v3)
+{
+  typedef typename viennagrid::result_of::line_handle<MeshT>::type LineHandleType;
+  viennagrid::static_array<LineHandleType, 4> lines;
+  lines[0] = get_make_line(mesh, vertex_line_map, v0, v1);
+  lines[1] = get_make_line(mesh, vertex_line_map, v1, v2);
+  lines[2] = get_make_line(mesh, vertex_line_map, v2, v3);
+  lines[3] = get_make_line(mesh, vertex_line_map, v3, v0);
+
+  return viennagrid::make_plc(mesh, lines.begin(), lines.end());
+}
+
+
+// this method helps us finding the seed point of a geometry based on a range of boundary PLCs
+template<typename MeshT, typename PLCHandleIteratorT>
+typename viennagrid::result_of::point<MeshT>::type get_seed_point( MeshT const & mesh, PLCHandleIteratorT const & begin, PLCHandleIteratorT const & end)
+{
+  // creating a temporary mesh parameter for the seed point locator
+  typename viennamesh::result_of::parameter_handle< MeshT >::type tmp_mesh = viennamesh::make_parameter<MeshT>();
+
+  // copy all PLCs to our temporary mesh
+  viennagrid::copy_element_handles(mesh, begin, end, tmp_mesh(), 0.0);
+
+  // Create the algorithm, set the temporary mesh as an input and execute it
+  viennamesh::algorithm_handle seed_point_locator( new viennamesh::seed_point_locator::algorithm() );
+  seed_point_locator->set_input( "default", tmp_mesh );
+  seed_point_locator->run();
+
+  // Querying the algorithm output (a seed point) and return it
+  typedef typename viennagrid::result_of::point<MeshT>::type PointType;
+  typedef typename viennamesh::result_of::point_container<PointType>::type PointContainerType;
+  typename viennamesh::result_of::parameter_handle<PointContainerType>::type point_container = seed_point_locator->get_output<PointContainerType>( "default" );
+  if (point_container && !point_container().empty())
+    return point_container()[0];
+
+  // something went wront...
+  return PointType();
+}
+
+int main()
+{
+  // creating an algorithm using the Tetgen meshing library for meshing a hull
+  viennamesh::algorithm_handle mesher( new viennamesh::tetgen::algorithm() );
+
+  // creating an algorithm for writing a mesh to a file
+  viennamesh::algorithm_handle writer( new viennamesh::file_writer() );
+
+
+  // typedefing some needed type for creating the geometry
+  typedef viennagrid::plc_3d_mesh GeometryMeshType;
+
+  typedef viennagrid::result_of::point<GeometryMeshType>::type PointType;
+  typedef viennagrid::result_of::vertex_handle<GeometryMeshType>::type GeometryVertexHandleType;
+  typedef viennagrid::result_of::vertex_id<GeometryMeshType>::type GeometryVertexIDType;
+  typedef viennagrid::result_of::line_handle<GeometryMeshType>::type GeometryLineHandleType;
+  typedef viennagrid::result_of::plc_handle<GeometryMeshType>::type GeometryPLCHandleType;
+
+  // creating the geometry mesh
+  viennamesh::result_of::parameter_handle< GeometryMeshType >::type geometry_handle = viennamesh::make_parameter<GeometryMeshType>();
+  GeometryMeshType & geometry = geometry_handle();
+
+
+  // creating a geometry with 3 cubes next to each other:
+  // +-----+-----+-----+
+  // |     |     |     |
+  // |  1  |  2  |  3  |
+  // |     |     |     |
+  // +-----+-----+-----+
+  double s = 10.0;
+  viennagrid::static_array<GeometryVertexHandleType, 16> vtx;
+
+  // creating all points
+  vtx[0] = viennagrid::make_vertex( geometry, PointType(0, 0, 0) );
+  vtx[1] = viennagrid::make_vertex( geometry, PointType(0, s, 0) );
+  vtx[2] = viennagrid::make_vertex( geometry, PointType(s, 0, 0) );
+  vtx[3] = viennagrid::make_vertex( geometry, PointType(s, s, 0) );
+
+  vtx[4] = viennagrid::make_vertex( geometry, PointType(0, 0, s) );
+  vtx[5] = viennagrid::make_vertex( geometry, PointType(0, s, s) );
+  vtx[6] = viennagrid::make_vertex( geometry, PointType(s, 0, s) );
+  vtx[7] = viennagrid::make_vertex( geometry, PointType(s, s, s) );
+
+  vtx[8] = viennagrid::make_vertex( geometry, PointType(0, 0, 2*s) );
+  vtx[9] = viennagrid::make_vertex( geometry, PointType(0, s, 2*s) );
+  vtx[10] = viennagrid::make_vertex( geometry, PointType(s, 0, 2*s) );
+  vtx[11] = viennagrid::make_vertex( geometry, PointType(s, s, 2*s) );
+
+  vtx[12] = viennagrid::make_vertex( geometry, PointType(0, 0, 3*s) );
+  vtx[13] = viennagrid::make_vertex( geometry, PointType(0, s, 3*s) );
+  vtx[14] = viennagrid::make_vertex( geometry, PointType(s, 0, 3*s) );
+  vtx[15] = viennagrid::make_vertex( geometry, PointType(s, s, 3*s) );
+
+
+
+  // We are using this map to keep track of all lines we use, this method is used by get_make_line which is used by make_quad_plc
+  std::map< std::pair<GeometryVertexIDType, GeometryVertexIDType>, GeometryLineHandleType > vertex_line_map;
+  viennagrid::static_array<GeometryPLCHandleType, 16> plcs;
+
+  // the top quad of the first cube
+  plcs[0] = make_quad_plc( geometry, vertex_line_map, vtx[0], vtx[1], vtx[3], vtx[2] );
+
+  // the side quads of the first cube
+  plcs[1] = make_quad_plc( geometry, vertex_line_map, vtx[0], vtx[4], vtx[5], vtx[1] );
+  plcs[2] = make_quad_plc( geometry, vertex_line_map, vtx[2], vtx[6], vtx[7], vtx[3] );
+  plcs[3] = make_quad_plc( geometry, vertex_line_map, vtx[0], vtx[4], vtx[6], vtx[2] );
+  plcs[4] = make_quad_plc( geometry, vertex_line_map, vtx[1], vtx[5], vtx[7], vtx[3] );
+
+  // the bottom quad of the first cube which is also the top quad of the second cube
+  plcs[5] = make_quad_plc( geometry, vertex_line_map, vtx[4], vtx[5], vtx[7], vtx[6] );
+
+  // the side quads of the second cube
+  plcs[6] = make_quad_plc( geometry, vertex_line_map, vtx[4], vtx[8], vtx[9], vtx[5] );
+  plcs[7] = make_quad_plc( geometry, vertex_line_map, vtx[6], vtx[10], vtx[11], vtx[7] );
+  plcs[8] = make_quad_plc( geometry, vertex_line_map, vtx[4], vtx[8], vtx[10], vtx[6] );
+  plcs[9] = make_quad_plc( geometry, vertex_line_map, vtx[5], vtx[9], vtx[11], vtx[7] );
+
+  // the bottom quad of the second cube which is also the top quad of the third cube
+  plcs[10] = make_quad_plc( geometry, vertex_line_map, vtx[8], vtx[9], vtx[11], vtx[10] );
+
+  // the side quads of the third cube
+  plcs[11] = make_quad_plc( geometry, vertex_line_map, vtx[8], vtx[12], vtx[13], vtx[9] );
+  plcs[12] = make_quad_plc( geometry, vertex_line_map, vtx[10], vtx[14], vtx[15], vtx[11] );
+  plcs[13] = make_quad_plc( geometry, vertex_line_map, vtx[8], vtx[12], vtx[14], vtx[10] );
+  plcs[14] = make_quad_plc( geometry, vertex_line_map, vtx[9], vtx[13], vtx[15], vtx[11] );
+
+  // the bottom quad of the third cube
+  plcs[15] = make_quad_plc( geometry, vertex_line_map, vtx[12], vtx[13], vtx[15], vtx[14] );
+
+
+  // extracting the seed points for each cube based on their boundary PLCs
+  PointType seed_point_cube0 = get_seed_point( geometry, plcs.begin()+0, plcs.begin()+6 );
+  PointType seed_point_cube1 = get_seed_point( geometry, plcs.begin()+5, plcs.begin()+11 );
+  PointType seed_point_cube2 = get_seed_point( geometry, plcs.begin()+10, plcs.begin()+16 );
+
+
+  // set the input geometry and seed points
+  mesher->set_input( "default", geometry_handle );
+  viennamesh::seed_point_3d_container seed_points;
+  seed_points.push_back( std::make_pair( seed_point_cube0, 0 ) );
+  seed_points.push_back( std::make_pair( seed_point_cube1, 1 ) );
+  seed_points.push_back( std::make_pair( seed_point_cube2, 2 ) );
+
+  mesher->set_input( "seed_points", seed_points );
+
+
+  // setting the mesher paramters
+  mesher->set_input( "cell_size", 1.0 );              // maximum cell size
+  mesher->set_input( "max_radius_edge_ratio", 1.5 );  // maximum radius edge ratio
+  mesher->set_input( "min_dihedral_angle", 0.17 );     // minimum dihedral angle in radiant, 0.17 are about 10 degrees
+
+  // linking the output from the mesher to the writer
+  writer->link_input( "default", mesher, "default" );
+
+
+  // Setting the filename for the reader and writer
+  writer->set_input( "filename", "three_cubes.vtu" );
+
+  // start the algorithms
+  mesher->run();
+  writer->run();
+
+
+}
