c3e85eeba76075b6ef1bd2e07b3ded684cf57363
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-11-26 13:30:59 +0100

    added extract_filename and extract_path

--- viennamesh/utils/string_tools.hpp
+++ viennamesh/utils/string_tools.hpp
@@ -67,6 +67,28 @@ namespace stringtools
     return tmp;
   }
 
+
+
+  inline std::string extract_filename( std::string const & path )
+  {
+    std::string path_delimiters = "\\/";
+
+    std::size_t pos = path.find_last_of(path_delimiters);
+    if (pos == std::string::npos)
+      return path;
+    return path.substr(pos+1, std::string::npos);
+  }
+
+  inline std::string extract_path( std::string const & path )
+  {
+    std::string path_delimiters = "\\/";
+
+    std::size_t pos = path.find_last_of(path_delimiters);
+    if (pos == std::string::npos)
+      return "";
+    return path.substr(0, pos+1);
+  }
+
 }
 
 #endif
