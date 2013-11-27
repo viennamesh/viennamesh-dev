52dbbb83bcec0a0b46e23929a47d30acfc9d7c2f
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-11-27 16:11:24 +0100

    added line coearsening, laplace_smooth and linear_transform algorithm

--- viennamesh/algorithm/viennagrid.hpp
+++ viennamesh/algorithm/viennagrid.hpp
@@ -1,6 +1,11 @@
 #ifndef VIENNAMESH_ALGORITHM_VIENNAGRID_HPP
 #define VIENNAMESH_ALGORITHM_VIENNAGRID_HPP
 
+#include "viennamesh/algorithm/viennagrid/linear_transform.hpp"
+#include "viennamesh/algorithm/viennagrid/laplace_smooth.hpp"
+
+#include "viennamesh/algorithm/viennagrid/line_coarsening.hpp"
+
 #include "viennamesh/algorithm/viennagrid/extract_hull.hpp"
 //#include "viennamesh/algorithm/viennagrid/extract_plc.hpp"
 
