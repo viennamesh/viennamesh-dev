77061bee6f65e33583a4ea8973e9b8c3cea3c581
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-12-02 15:42:07 +0100

    added extract seed points algorithm

--- viennamesh/algorithm/viennagrid.hpp
+++ viennamesh/algorithm/viennagrid.hpp
@@ -6,6 +6,7 @@
 
 #include "viennamesh/algorithm/viennagrid/line_coarsening.hpp"
 
+#include "viennamesh/algorithm/viennagrid/extract_seed_points.hpp"
 #include "viennamesh/algorithm/viennagrid/extract_hull.hpp"
 //#include "viennamesh/algorithm/viennagrid/extract_plc.hpp"
 
