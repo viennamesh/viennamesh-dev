ce371e57d3e327e4c4799e12822fa431daf9f7f5
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-12-02 15:47:35 +0100

    Added meshing parameters

--- examples/tutorial/netgen_csg_mesher.cpp
+++ examples/tutorial/netgen_csg_mesher.cpp
@@ -24,7 +24,24 @@ int main()
 
 
   // setting the mesher paramters
-  mesher->set_input( "default", csg_string );              // the CSG string
+  mesher->set_input( "default", csg_string );               // the CSG string
+
+  mesher->set_input( "delaunay", true );                    // use delaunay meshing
+  mesher->set_input( "cell_size", 0.1 );                    // set the cell size
+  mesher->set_input( "grading", 0.3 );                      // set the element grading, 0...1 (0 => uniform mesh; 1 => aggressive local grading)
+  mesher->set_input( "optimization_steps", 3 );             // set the number of optimization steps for 3-D mesh optimization
+  mesher->set_input( "optimize_string", "cmdmustm" );       // set the optimization strategy:
+                                                            // m .. move nodes
+                                                            // M .. move nodes, cheap functional
+                                                            // s .. swap faces
+                                                            // t .. swap and improve faces
+                                                            // c .. combine elements
+                                                            // d .. divide elements
+                                                            // p .. plot, no pause
+                                                            // P .. plot, Pause
+                                                            // h .. Histogramm, no pause
+                                                            // H .. Histogramm, pause
+                                                            // t .. ignored
 
   // linking the output from the mesher to the writer
   writer->link_input( "default", mesher, "default" );
