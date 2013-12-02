0418ebb7a9c031245cd79af61362bf77397ef273
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-12-02 15:41:44 +0100

    added meshing parameters

--- viennamesh/algorithm/netgen/csg_generator.hpp
+++ viennamesh/algorithm/netgen/csg_generator.hpp
@@ -25,10 +25,25 @@ namespace viennamesh
         std::istringstream csg_stream(csg_string());
         ::netgen::CSGeometry * geom = ::netgen::ParseCSG( csg_stream );
 
-        geom->FindIdenticSurfaces(1e-8 * geom->MaxSize());
-        ::netgen::MeshingParameters mparams;
+        double relative_find_identic_surface_eps = 1e-8;
+        copy_input( "relative_find_identic_surface_eps", relative_find_identic_surface_eps );
 
-        geom->GenerateMesh(output_mesh().mesh, mparams, 1, 5);
+        geom->FindIdenticSurfaces(relative_find_identic_surface_eps * geom->MaxSize());
+        ::netgen::MeshingParameters mesh_parameters;
+
+        copy_input( "cell_size", mesh_parameters.maxh );
+        copy_input( "grading", mesh_parameters.grading );
+        copy_input( "optimization_steps", mesh_parameters.optsteps3d );
+
+        const_bool_parameter_handle delaunay = get_input<bool>( "delaunay" );
+        if (delaunay)
+          mesh_parameters.delaunay = delaunay();
+
+        const_string_parameter_handle optimize_string = get_input<string>( "optimize_string" );
+        if (optimize_string)
+          mesh_parameters.optimize3d = optimize_string().c_str();
+
+        geom->GenerateMesh(output_mesh().mesh, mesh_parameters, 1, 5);
 
         delete geom;
 
