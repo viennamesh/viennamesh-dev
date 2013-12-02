6502a59fcbac8e3361053dd3a6d96ab2f449cd6d
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-12-02 16:36:45 +0100

    meshing deva geometry

--- examples/tutorial/mesh_yannick.cpp
+++ examples/tutorial/mesh_yannick.cpp
@@ -1,5 +1,6 @@
 #include "viennamesh/algorithm/viennagrid.hpp"
 #include "viennamesh/algorithm/io.hpp"
+#include "viennamesh/algorithm/triangle.hpp"
 
 
 int main()
@@ -8,33 +9,29 @@ int main()
   viennamesh::algorithm_handle reader( new viennamesh::io::mesh_reader() );
 
   // creating a hull extraction algorithm
-  viennamesh::algorithm_handle extract_hull( new viennamesh::extract_hull::algorithm() );
-  viennamesh::algorithm_handle linear_transform( new viennamesh::linear_transform::algorithm() );
+  viennamesh::algorithm_handle mesher( new viennamesh::triangle::algorithm() );
 
   // creating an algorithm for writing a mesh to a file
   viennamesh::algorithm_handle writer( new viennamesh::io::mesh_writer() );
 
 
   // linking the output from the reader to the mesher
-  extract_hull->link_input( "default", reader, "default" );
-  linear_transform->link_input( "default", extract_hull, "default" );
-  writer->link_input( "default", linear_transform, "default" );
+  mesher->link_input( "default", reader, "default" );
+  writer->link_input( "default", mesher, "default" );
 
 
   // Setting the filename for the reader and writer
-  reader->set_input( "filename", "/export/florian/work/projects/2013_11 ViennaSHE Yannick/mesh/ridiculously_fine_ortho_for_florian_out.devbz.vtu_main.pvd" );
+  reader->set_input( "filename", "/export/florian/work/projects/2013_11 ViennaSHE Yannick/geometry.deva" );
 
-  std::vector<double> matrix(3*2, 0);
-  matrix[0*3+0] = 1;
-  matrix[1*3+1] = 1;
-  linear_transform->set_input( "matrix", matrix );
+  mesher->set_input( "min_angle", 0.35 );     // minimum angle in radiant, 0.35 are about 20 degrees
+  mesher->set_input( "delaunay", true  );     // we want a Delaunay triangulation
+  mesher->set_input( "algorithm_type", "incremental_delaunay" );  // incremental Delaunay algorithm is used
 
-  writer->set_input( "filename", "nld_mosfet_lines.vtu" );
+  writer->set_input( "filename", "nld_mosfet.vtu" );
 
   // start the algorithms
   reader->run();
-  extract_hull->run();
-  linear_transform->run();
+  mesher->run();
   writer->run();
 
 
