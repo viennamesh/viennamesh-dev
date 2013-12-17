15d1e29f832666a437224be6b8aa93d349b932e4
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-12-17 11:00:48 +0100

    changed from static_init to type_information, added name(), added type information

--- viennamesh/core/basic_parameters.hpp
+++ viennamesh/core/basic_parameters.hpp
@@ -1,6 +1,8 @@
 #ifndef VIENNAMESH_CORE_BASIC_PARAMETERS_HPP
 #define VIENNAMESH_CORE_BASIC_PARAMETERS_HPP
 
+#include <sstream>
+
 #include "viennagrid/config/default_configs.hpp"
 #include "viennagrid/mesh/segmented_mesh.hpp"
 #include "viennamesh/core/parameter.hpp"
@@ -8,66 +10,120 @@
 namespace viennamesh
 {
   template<>
-  struct static_init_impl<bool>
+  struct type_information<bool>
   {
     static void init()
     {
-      converter::get().register_conversion<bool, int>( &static_cast_convert<bool, int> );
+//       converter::get().register_conversion<bool, int>( &static_cast_convert<bool, int> );
+    }
+
+    static string name()
+    {
+      return "bool";
     }
   };
 
   template<>
-  struct static_init_impl<int>
+  struct type_information<int>
   {
     static void init()
     {
       converter::get().register_conversion<int, double>( &static_cast_convert<int, double> );
-      converter::get().register_conversion<int, bool>( &static_cast_convert<int, bool> );
+//       converter::get().register_conversion<int, bool>( &static_cast_convert<int, bool> );
+    }
+
+    static string name()
+    {
+      return "int";
     }
   };
 
   template<>
-  struct static_init_impl<double>
+  struct type_information<double>
   {
     static void init()
     {
       converter::get().register_conversion<double, int>( &static_cast_convert<double, int> );
     }
+
+    static string name()
+    {
+      return "double";
+    }
   };
 
 
   template<>
-  struct static_init_impl<string>
+  struct type_information<string>
   {
     typedef string SelfT;
 
-    static void init()
+    static void init() {}
+
+    static string name()
     {
-      info(10) << "static_init<string>::init" << std::endl;
+      return "string";
     }
   };
 
   template<typename WrappedMeshConfigT, typename WrappedSegmentationConfigT>
-  struct static_init_impl< viennagrid::segmented_mesh<viennagrid::mesh<WrappedMeshConfigT>, viennagrid::segmentation<WrappedSegmentationConfigT> > >
+  struct type_information< viennagrid::segmented_mesh<viennagrid::mesh<WrappedMeshConfigT>, viennagrid::segmentation<WrappedSegmentationConfigT> > >
   {
-    typedef viennagrid::segmented_mesh<viennagrid::mesh<WrappedMeshConfigT>, viennagrid::segmentation<WrappedSegmentationConfigT> > SelfT;
+    typedef viennagrid::mesh<WrappedMeshConfigT> MeshType;
+    typedef viennagrid::segmentation<WrappedSegmentationConfigT> SegmentationType;
+    typedef typename viennagrid::result_of::cell_tag<MeshType>::type CellTag;
+
+    typedef viennagrid::segmented_mesh<MeshType, SegmentationType> SelfType;
 
     static void init()
     {
-      type_properties::get().set_property<SelfT>( "is_mesh", "true" );
-      type_properties::get().set_property<SelfT>( "is_viennagrid", "true" );
+      type_properties::get().set_property<SelfType>( "is_mesh", "true" );
+      type_properties::get().set_property<SelfType>( "is_viennagrid", "true" );
+      type_properties::get().set_property<SelfType>( "is_segmented", "true" );
+
+      int geometric_dimension = viennagrid::result_of::geometric_dimension<MeshType>::value;
+      type_properties::get().set_property<SelfType>( "geometric_dimension", boost::lexical_cast<string>(geometric_dimension) );
+      type_properties::get().set_property<SelfType>( "cell_type", CellTag::name() );
+
+      info(1) << "init: cell_type = " << CellTag::name() << std::endl;
+    }
+
+    static string name()
+    {
+      std::stringstream ss;
+      ss << "viennagrid::segmented_mesh< cell type = " << CellTag::name() << ", geometric dimension = " << viennagrid::result_of::geometric_dimension<MeshType>::value << ">";
+
+      return ss.str();
     }
   };
 
   template<typename WrappedMeshConfigT>
-  struct static_init_impl< viennagrid::mesh<WrappedMeshConfigT> >
+  struct type_information< viennagrid::mesh<WrappedMeshConfigT> >
   {
-    typedef viennagrid::mesh<WrappedMeshConfigT> SelfT;
+    typedef viennagrid::mesh<WrappedMeshConfigT> MeshType;
+    typedef typename viennagrid::result_of::cell_tag<MeshType>::type CellTag;
+
+    typedef MeshType SelfType;
 
     static void init()
     {
-      type_properties::get().set_property<SelfT>( "is_mesh", "true" );
-      type_properties::get().set_property<SelfT>( "is_viennagrid", "true" );
+      type_properties::get().set_property<SelfType>( "is_mesh", "true" );
+      type_properties::get().set_property<SelfType>( "is_viennagrid", "true" );
+      type_properties::get().set_property<SelfType>( "is_segmented", "false" );
+
+      int geometric_dimension = viennagrid::result_of::geometric_dimension<MeshType>::value;
+      type_properties::get().set_property<SelfType>( "geometric_dimension", boost::lexical_cast<string>(geometric_dimension) );
+      type_properties::get().set_property<SelfType>( "cell_type", CellTag::name() );
+
+      info(1) << "init: cell_type = " << CellTag::name() << std::endl;
+    }
+
+    static string name()
+    {
+      std::stringstream ss;
+      ss << "viennagrid::mesh< cell type = " << CellTag::name() << ", geometric dimension = " << viennagrid::result_of::geometric_dimension<MeshType>::value << ">";
+
+      return ss.str();
     }
   };
 
