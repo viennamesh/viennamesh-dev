2a5ad1474c066a185e6d3d1c3e7f74647ca225ec
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-01-27 16:30:39 +0100

    Added BND to VTK conversion, added bnd mesher

---
+++ examples/tutorial/bnd_to_vtk.cpp
@@ -0,0 +1,27 @@
+#include "viennagrid/config/default_configs.hpp"
+#include "viennagrid/io/bnd_reader.hpp"
+#include "viennagrid/io/vtk_writer.hpp"
+
+
+int main( int argc, char** argv )
+{
+  if (argc != 3)
+  {
+    std::cout << "Usage: bnd_to_vtk [input.bnd] [output]" << std::endl;
+    return 0;
+  }
+
+  typedef viennagrid::segmented_mesh<viennagrid::triangular_3d_mesh, viennagrid::triangular_3d_segmentation> SegmentedTriangleMeshType;
+
+  viennamesh::result_of::parameter_handle<SegmentedTriangleMeshType>::type triangle_mesh = viennamesh::make_parameter<SegmentedTriangleMeshType>();
+
+  {
+    viennagrid::io::bnd_reader reader;
+    reader( triangle_mesh().mesh, triangle_mesh().segmentation, argv[1] );
+  }
+
+  {
+    viennagrid::io::vtk_writer<viennagrid::triangular_3d_mesh> writer;
+    writer( triangle_mesh().mesh, triangle_mesh().segmentation, argv[2] );
+  }
+}
