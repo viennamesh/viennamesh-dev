7d6b304bad69eec6895cf7e73e562e72e6b67987
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-02-25 14:43:39 +0100

    added new algorithms

--- viennamesh/algorithm/viennagrid.hpp
+++ viennamesh/algorithm/viennagrid.hpp
@@ -2,13 +2,16 @@
 #define VIENNAMESH_ALGORITHM_VIENNAGRID_HPP
 
 #include "viennamesh/algorithm/viennagrid/linear_transform.hpp"
+#include "viennamesh/algorithm/viennagrid/project.hpp"
 #include "viennamesh/algorithm/viennagrid/laplace_smooth.hpp"
 
 #include "viennamesh/algorithm/viennagrid/line_coarsening.hpp"
+// #include "viennamesh/algorithm/viennagrid/line_cut.hpp"
+
+#include "viennamesh/algorithm/viennagrid/hyperplane_clip.hpp"
 
 #include "viennamesh/algorithm/viennagrid/extract_seed_points.hpp"
 #include "viennamesh/algorithm/viennagrid/extract_hull.hpp"
-//#include "viennamesh/algorithm/viennagrid/extract_plc.hpp"
 
 #include "viennamesh/algorithm/viennagrid/refine.hpp"
 
