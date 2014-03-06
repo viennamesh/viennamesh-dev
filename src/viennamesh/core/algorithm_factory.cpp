9a4d8415192d5356f99ba9473792a318ac727db5
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-03-06 16:46:23 +0100

    added algorithm entries

--- src/viennamesh/core/algorithm_factory.cpp
+++ src/viennamesh/core/algorithm_factory.cpp
@@ -1,7 +1,33 @@
 #include "viennamesh/core/algorithm_factory.hpp"
 
+
+#include "viennamesh/algorithm/io.hpp"
+#include "viennamesh/algorithm/triangle.hpp"
+#include "viennamesh/algorithm/tetgen.hpp"
+#include "viennamesh/algorithm/viennagrid.hpp"
+
+
 namespace viennamesh
 {
+
+  algorithm_factory_t::algorithm_factory_t()
+  {
+    register_algorithm<viennamesh::io::mesh_reader>("mesh_reader");
+    register_algorithm<viennamesh::io::mesh_writer>("mesh_writer");
+
+    register_algorithm<viennamesh::triangle::algorithm>("generate_triangle");
+    register_algorithm<viennamesh::tetgen::algorithm>("generate_tetgen");
+
+    register_algorithm<viennamesh::affine_transform::algorithm>("affine_transform");
+    register_algorithm<viennamesh::extract_hull::algorithm>("extract_hull");
+    register_algorithm<viennamesh::hyperplane_clip::algorithm>("hyperplane_clip");
+
+    register_algorithm<viennamesh::hyperplane_clip::algorithm>("map_segments");
+    register_algorithm<viennamesh::hyperplane_clip::algorithm>("merge_segments");
+    register_algorithm<viennamesh::hyperplane_clip::algorithm>("line_coarsening");
+  }
+
+
   algorithm_factory_t & algorithm_factory()
   {
     static algorithm_factory_t factory_;
