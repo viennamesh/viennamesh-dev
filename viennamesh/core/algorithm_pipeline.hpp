9b4f0f0af6f3708d3683ab1923985166878e5e9d
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-03-06 17:15:17 +0100

    fixes (dynamic parameter name and type, attributes for parameters), log output

--- viennamesh/core/algorithm_pipeline.hpp
+++ viennamesh/core/algorithm_pipeline.hpp
@@ -20,53 +20,87 @@ namespace viennamesh
     {
       pugi::xml_attribute algorithm_name_attribute = algorithm_node.attribute("name");
       if ( algorithm_name_attribute.empty() )
+      {
+        error(1) << "Algorithm has no name attribute" << std::endl;
         return false;
+      }
       string algorithm_name = algorithm_name_attribute.as_string();
 
       pugi::xml_attribute algorithm_type_attribute = algorithm_node.attribute("type");
       if ( algorithm_type_attribute.empty() )
+      {
+        error(1) << "Algorithm \"" << algorithm_name << "\" has no type attribute" << std::endl;
         return false;
+      }
       string algorithm_type = algorithm_type_attribute.as_string();
 
       algorithm_handle algorithm = viennamesh::algorithm_factory().create_from_name( algorithm_type );
 
       if (!algorithm)
+      {
+        error(1) << "Algorithm creation from factory failed" << std::endl;
         return false;
+      }
 
       for (pugi::xml_node paramater_node = algorithm_node.child("parameter");
            paramater_node;
            paramater_node = paramater_node.next_sibling("parameter"))
       {
-        pugi::xml_attribute parameter_name_attribute = algorithm_node.attribute("name");
+        pugi::xml_attribute parameter_name_attribute = paramater_node.attribute("name");
         if (parameter_name_attribute.empty())
+        {
+          error(1) << "Parameter has no name attribute" << std::endl;
           return false;
+        }
         string parameter_name = parameter_name_attribute.as_string();
 
-        pugi::xml_attribute parameter_type_attribute = algorithm_node.attribute("type");
+        pugi::xml_attribute parameter_type_attribute = paramater_node.attribute("type");
         if (parameter_type_attribute.empty())
+        {
+          error(1) << "Parameter \"" << parameter_name << "\" has no type attribute" << std::endl;
           return false;
+        }
         string parameter_type = parameter_type_attribute.as_string();
 
 
         string parameter_value = paramater_node.text().as_string();
         if (parameter_value.empty())
+        {
+          error(1) << "Parameter \"" << parameter_name << "\" has no value" << std::endl;
           return false;
+        }
 
 
         if (parameter_type == "string")
+        {
           algorithm->set_input( parameter_name, parameter_value );
+        }
         else if (parameter_type == "double")
+        {
           algorithm->set_input( parameter_name, lexical_cast<double>(parameter_value) );
+        }
+        else if (parameter_type == "point")
+        {
+          algorithm->set_input( parameter_name, stringtools::vector_from_string<double>(parameter_value) );
+        }
         else if (parameter_type == "dynamic")
         {
-          string source_algorithm_name = parameter_value.substr( 0, parameter_value.find("/")-1 );
-          string source_parameter_name = parameter_value.substr( parameter_value.find("/") );
+          string source_algorithm_name = parameter_value.substr( 0, parameter_value.find("/") );
+          string source_parameter_name = parameter_value.substr( parameter_value.find("/")+1 );
 
           std::map<string, algorithm_handle>::iterator ait = algorithms.find( source_algorithm_name );
           if (ait == algorithms.end())
+          {
+            error(1) << "Dynamic parameter \"" << parameter_name << "\": algorithm \"" << source_algorithm_name << "\" was not found" << std::endl;
             return false;
+          }
 
           algorithm->set_input( parameter_name, make_parameter_link(ait->second, source_parameter_name) );
+        }
+        else
+        {
+          error(1) << "Parameter \"" << parameter_name << "\": type \"" << parameter_type << "\" is not supported" << std::endl;
+          return false;
         }
 
       }
@@ -76,13 +110,12 @@ namespace viennamesh
         algorithms.insert( std::make_pair(algorithm_name, algorithm) );
 
       if (!result.second)
+      {
+        error(1) << "Duplicated algorithm with \"" << algorithm_name << "\" and type \"" << algorithm_type << std::endl;
         return false;
+      }
 
       algorithm_order.push_back( result.first );
-
-//       algorithms[algorithm_name] = algorithm;
-//       algorithm_order
-//         functions.push_back( from_xml(source.first_child(), mesh, segmentation) );
 
       return true;
     }
@@ -93,7 +126,11 @@ namespace viennamesh
            algorithm_node;
            algorithm_node = algorithm_node.next_sibling("algorithm"))
       {
-        add_algorithm( algorithm_node );
+        if (!add_algorithm( algorithm_node ))
+        {
+          clear();
+          return false;
+        }
       }
 
       return true;
