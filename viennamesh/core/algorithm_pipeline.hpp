7901f746297dbd8cff40a6815e31ebee491b5b10
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-03-10 10:20:49 +0100

    moved implementation to cpp

--- viennamesh/core/algorithm_pipeline.hpp
+++ viennamesh/core/algorithm_pipeline.hpp
@@ -2,8 +2,6 @@
 #define VIENNAMESH_CORE_ALGORITHM_PIPELINE_HPP
 
 #include "viennamesh/core/algorithm.hpp"
-#include "viennamesh/core/algorithm_factory.hpp"
-
 #include "pugixml/pugixml.hpp"
 
 namespace viennamesh
@@ -16,141 +14,11 @@ namespace viennamesh
     typedef std::map<string, algorithm_handle> AlgorithmMapType;
 
 
-    bool add_algorithm( pugi::xml_node const & algorithm_node )
-    {
-      pugi::xml_attribute algorithm_name_attribute = algorithm_node.attribute("name");
-      if ( algorithm_name_attribute.empty() )
-      {
-        error(1) << "Algorithm has no name attribute" << std::endl;
-        return false;
-      }
-      string algorithm_name = algorithm_name_attribute.as_string();
+    bool add_algorithm( pugi::xml_node const & algorithm_node );
+    bool from_xml( pugi::xml_node const & xml );
+    bool run();
 
-      pugi::xml_attribute algorithm_type_attribute = algorithm_node.attribute("type");
-      if ( algorithm_type_attribute.empty() )
-      {
-        error(1) << "Algorithm \"" << algorithm_name << "\" has no type attribute" << std::endl;
-        return false;
-      }
-      string algorithm_type = algorithm_type_attribute.as_string();
-
-      algorithm_handle algorithm = viennamesh::algorithm_factory().create_from_name( algorithm_type );
-
-      if (!algorithm)
-      {
-        error(1) << "Algorithm creation from factory failed" << std::endl;
-        return false;
-      }
-
-      for (pugi::xml_node paramater_node = algorithm_node.child("parameter");
-           paramater_node;
-           paramater_node = paramater_node.next_sibling("parameter"))
-      {
-        pugi::xml_attribute parameter_name_attribute = paramater_node.attribute("name");
-        if (parameter_name_attribute.empty())
-        {
-          error(1) << "Parameter has no name attribute" << std::endl;
-          return false;
-        }
-        string parameter_name = parameter_name_attribute.as_string();
-
-        pugi::xml_attribute parameter_type_attribute = paramater_node.attribute("type");
-        if (parameter_type_attribute.empty())
-        {
-          error(1) << "Parameter \"" << parameter_name << "\" has no type attribute" << std::endl;
-          return false;
-        }
-        string parameter_type = parameter_type_attribute.as_string();
-
-
-        string parameter_value = paramater_node.text().as_string();
-        if (parameter_value.empty())
-        {
-          error(1) << "Parameter \"" << parameter_name << "\" has no value" << std::endl;
-          return false;
-        }
-
-
-        if (parameter_type == "string")
-        {
-          algorithm->set_input( parameter_name, parameter_value );
-        }
-        else if (parameter_type == "double")
-        {
-          algorithm->set_input( parameter_name, lexical_cast<double>(parameter_value) );
-        }
-        else if (parameter_type == "point")
-        {
-          algorithm->set_input( parameter_name, stringtools::vector_from_string<double>(parameter_value) );
-        }
-        else if (parameter_type == "dynamic")
-        {
-          string source_algorithm_name = parameter_value.substr( 0, parameter_value.find("/") );
-          string source_parameter_name = parameter_value.substr( parameter_value.find("/")+1 );
-
-          std::map<string, algorithm_handle>::iterator ait = algorithms.find( source_algorithm_name );
-          if (ait == algorithms.end())
-          {
-            error(1) << "Dynamic parameter \"" << parameter_name << "\": algorithm \"" << source_algorithm_name << "\" was not found" << std::endl;
-            return false;
-          }
-
-          algorithm->set_input( parameter_name, make_parameter_link(ait->second, source_parameter_name) );
-        }
-        else
-        {
-          error(1) << "Parameter \"" << parameter_name << "\": type \"" << parameter_type << "\" is not supported" << std::endl;
-          return false;
-        }
-
-      }
-
-
-      std::pair<AlgorithmMapType::iterator, bool> result =
-        algorithms.insert( std::make_pair(algorithm_name, algorithm) );
-
-      if (!result.second)
-      {
-        error(1) << "Duplicated algorithm with \"" << algorithm_name << "\" and type \"" << algorithm_type << std::endl;
-        return false;
-      }
-
-      algorithm_order.push_back( result.first );
-
-      return true;
-    }
-
-    bool from_xml( pugi::xml_node const & xml )
-    {
-      for (pugi::xml_node algorithm_node = xml.child("algorithm");
-           algorithm_node;
-           algorithm_node = algorithm_node.next_sibling("algorithm"))
-      {
-        if (!add_algorithm( algorithm_node ))
-        {
-          clear();
-          return false;
-        }
-      }
-
-      return true;
-    }
-
-    bool run()
-    {
-      for (std::vector<AlgorithmMapType::iterator>::iterator ait = algorithm_order.begin(); ait != algorithm_order.end(); ++ait)
-      {
-        if (!(*ait)->second->run())
-          return false;
-      }
-
-      return true;
-    }
-
-    void clear()
-    {
-      algorithms.clear();
-    }
+    void clear();
 
   private:
 
--- viennamesh/core/parameter.hpp
+++ viennamesh/core/parameter.hpp
@@ -162,20 +162,7 @@ namespace viennamesh
       return type_properties_map[ti];
     }
 
-    std::pair<string, bool> get_property( type_info_wrapper const & ti, string const & name ) const
-    {
-      std::map<type_info_wrapper, PropertyMapType>::const_iterator tpit = type_properties_map.find(ti);
-      if (tpit != type_properties_map.end())
-      {
-        PropertyMapType::const_iterator pmit = tpit->second.find(name);
-        if (pmit != tpit->second.end())
-        {
-          return std::pair<string, bool>(pmit->second, true);
-        }
-      }
-
-      return std::pair<string, bool>("", false);
-    }
+    std::pair<string, bool> get_property( type_info_wrapper const & ti, string const & name ) const;
 
     void set_property( type_info_wrapper const & ti, string const & key, string const & value )
     {
@@ -188,59 +175,10 @@ namespace viennamesh
       set_property( typeid( parameter_wrapper<ValueT> ), key, value );
     }
 
-    bool match_properties( type_info_wrapper const & ti, PropertyMapType const & to_match ) const
-    {
-      if (to_match.empty())
-        return true;
+    bool match_properties( type_info_wrapper const & ti, PropertyMapType const & to_match ) const;
+    bool match_property( type_info_wrapper const & ti, string const & key, string const & value ) const;
 
-      std::map<type_info_wrapper, PropertyMapType>::const_iterator pmit = type_properties_map.find(ti);
-
-      if (pmit == type_properties_map.end())
-        return false;
-
-      PropertyMapType const & properties = pmit->second;
-
-      for (PropertyMapType::const_iterator it = to_match.begin(); it != to_match.end(); ++it)
-      {
-        PropertyMapType::const_iterator pit = properties.find( it->first );
-        if ( pit == properties.end() )
-          return false;
-
-        if ( it->second != pit->second )
-          return false;
-      }
-
-      return true;
-    }
-
-    bool match_property( type_info_wrapper const & ti, string const & key, string const & value ) const
-    {
-      std::map<type_info_wrapper, PropertyMapType>::const_iterator pmit = type_properties_map.find(ti);
-
-      if (pmit == type_properties_map.end())
-        return false;
-
-      PropertyMapType const & properties = pmit->second;
-
-      PropertyMapType::const_iterator pit = properties.find(key);
-      if ( pit == properties.end() )
-        return false;
-
-      return pit->second == value;
-    }
-
-
-    void print() const
-    {
-      for (std::map<type_info_wrapper, PropertyMapType>::const_iterator tit = type_properties_map.begin(); tit != type_properties_map.end(); ++tit)
-      {
-        std::cout << "Type: " << tit->first.name() << std::endl;
-        for (PropertyMapType::const_iterator pit = tit->second.begin(); pit != tit->second.end(); ++pit)
-        {
-          std::cout << "  " << pit->first << " - " << pit->second << std::endl;
-        }
-      }
-    }
+    void print() const;
 
 
     static type_properties & get()
@@ -508,19 +446,11 @@ namespace viennamesh
     }
   };
 
-
-
   inline bool is_convertable( const_parameter_handle const & source, parameter_handle & destination )
   { return converter::get().is_convertable( source, destination ); }
 
   inline bool convert( const_parameter_handle const & source, parameter_handle & destination )
   { return converter::get().convert( source, destination ); }
-
-
-
-
-
-
 
 
 
@@ -530,7 +460,6 @@ namespace viennamesh
     output = static_cast<OutputValueT>(input);
     return true;
   }
-
 
 
 
@@ -603,25 +532,6 @@ namespace viennamesh
 
 
 
-
-  inline type_info_wrapper converter::get_type_id( const_parameter_handle const & tmp )
-  { return typeid(*tmp); }
-
-  inline shared_ptr<base_conversion_function> converter::convert_function( const_parameter_handle const & input, const_parameter_handle const & output_mesh )
-  {
-    ConversionFunctionMapMapType::iterator ipit = conversions.find(typeid(*input));
-    if (ipit != conversions.end())
-    {
-      ConversionFunctionMapType::iterator opit = ipit->second.find(typeid(*output_mesh));
-      if ( opit != ipit->second.end() )
-      {
-        return opit->second;
-      }
-    }
-
-    return shared_ptr<base_conversion_function>();
-  }
-
   template<typename ValueT>
   shared_ptr<base_conversion_function> converter::convert_function( const_parameter_handle const & input )
   {
@@ -638,76 +548,6 @@ namespace viennamesh
     }
 
     return shared_ptr<base_conversion_function>();
-  }
-
-  inline shared_ptr<base_conversion_function> converter::best_convert_function( const_parameter_handle const & input, std::map<string, string> const & properties )
-  {
-    ConversionFunctionMapMapType::iterator ipit = conversions.find(typeid(*input));
-    if (ipit != conversions.end())
-    {
-      ConversionFunctionMapType::iterator best = ipit->second.end();
-      for (ConversionFunctionMapType::iterator opit = ipit->second.begin(); opit != ipit->second.end(); ++opit)
-      {
-        if ( type_properties::get().match_properties(opit->second->output_type(), properties) )
-        {
-          if (best == ipit->second.end())
-            best = opit;
-          else
-          {
-            if (best->second->function_depth > opit->second->function_depth)
-              best = opit;
-          }
-        }
-      }
-
-      if (best != ipit->second.end())
-        return best->second;
-    }
-
-    return shared_ptr<base_conversion_function>();
-  }
-
-  inline shared_ptr<base_conversion_function> converter::best_convert_function( const_parameter_handle const & input, string const & property_key, string const & property_value )
-  {
-    ConversionFunctionMapMapType::iterator ipit = conversions.find(typeid(*input));
-    if (ipit != conversions.end())
-    {
-      ConversionFunctionMapType::iterator best = ipit->second.end();
-      for (ConversionFunctionMapType::iterator opit = ipit->second.begin(); opit != ipit->second.end(); ++opit)
-      {
-        if ( type_properties::get().match_property(opit->second->output_type(), property_key, property_value) )
-        {
-          if (best == ipit->second.end())
-            best = opit;
-          else
-          {
-            if (best->second->function_depth > opit->second->function_depth)
-              best = opit;
-          }
-        }
-      }
-
-      if (best != ipit->second.end())
-        return best->second;
-    }
-
-    return shared_ptr<base_conversion_function>();
-  }
-
-
-
-  inline void converter::print_conversions(const_parameter_handle const & input) const
-  {
-    LoggingStack ls("Supported conversion functions");
-    info(10) << "Source type: [" << &typeid(*input) << "] " << std::endl;
-    ConversionFunctionMapMapType::const_iterator ipit = conversions.find(typeid(*input));
-    if (ipit != conversions.end())
-    {
-      for (ConversionFunctionMapType::const_iterator opit = ipit->second.begin(); opit != ipit->second.end(); ++opit)
-      {
-        info(10) << "Supports conversion to (" << opit->second->function_depth << "): [" << opit->first.get() << "] " << std::endl;
-      }
-    }
   }
 
 
