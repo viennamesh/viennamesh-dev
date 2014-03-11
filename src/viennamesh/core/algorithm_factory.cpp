2a44f398b7420909268142a38434e62559cf9067
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-03-11 16:41:12 +0100

    added string_reader and netgen_csg

--- src/viennamesh/core/algorithm_factory.cpp
+++ src/viennamesh/core/algorithm_factory.cpp
@@ -4,6 +4,7 @@
 #include "viennamesh/algorithm/io.hpp"
 #include "viennamesh/algorithm/triangle.hpp"
 #include "viennamesh/algorithm/tetgen.hpp"
+#include "viennamesh/algorithm/netgen.hpp"
 #include "viennamesh/algorithm/viennagrid.hpp"
 
 
@@ -13,10 +14,12 @@ namespace viennamesh
   algorithm_factory_t::algorithm_factory_t()
   {
     register_algorithm<viennamesh::io::mesh_reader>("mesh_reader");
+    register_algorithm<viennamesh::io::string_reader>("string_reader");
     register_algorithm<viennamesh::io::mesh_writer>("mesh_writer");
 
     register_algorithm<viennamesh::triangle::algorithm>("generate_triangle");
     register_algorithm<viennamesh::tetgen::algorithm>("generate_tetgen");
+    register_algorithm<viennamesh::netgen::csg_mesher>("netgen_csg");
 
     register_algorithm<viennamesh::affine_transform::algorithm>("affine_transform");
     register_algorithm<viennamesh::extract_hull::algorithm>("extract_hull");
