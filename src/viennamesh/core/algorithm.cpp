b2bc5035de613f7f83951f94baf4ed5cc97af0b4
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-03-11 14:17:18 +0100

    moved some implementations from header

--- src/viennamesh/core/algorithm.cpp
+++ src/viennamesh/core/algorithm.cpp
@@ -3,7 +3,33 @@
 namespace viennamesh
 {
 
-  
+  parameter_handle_t<parameter_link> make_parameter_link(algorithm_handle const & algorithm, string const & para_name)
+  {
+    return parameter_handle_t<parameter_link>( new parameter_link(algorithm, para_name) );
+  }
+
+  bool base_algorithm::run()
+  {
+    LoggingStack stack( string("Algoritm: ") + name() );
+    outputs.clear_non_references();
+
+    try
+    {
+      bool success = run_impl();
+
+      if (!success)
+        error(1) << "Algorithm failed" << std::endl;
+
+      return success;
+    }
+    catch ( algorithm_exception const & ex )
+    {
+      error(1) << "Algorithm failed!" << std::endl;
+      error(1) << ex.what() << std::endl;
+      return false;
+    }
+  }
+
 
 
 }
