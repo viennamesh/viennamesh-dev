e935848193ae1f2872cf09a43ef20772f0065d62
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-04-07 15:03:19 +0200

    directly using boost or std shared_ptr because of SWIG

--- viennamesh/core/algorithm.hpp
+++ viennamesh/core/algorithm.hpp
@@ -13,8 +13,15 @@ namespace viennamesh
 
   class base_algorithm;
 
-  typedef shared_ptr<base_algorithm> algorithm_handle;
-  typedef shared_ptr<const base_algorithm> const_algorithm_handle;
+#ifdef VIENNAMESH_USES_BOOST
+  typedef boost::shared_ptr<base_algorithm> algorithm_handle;
+  typedef boost::shared_ptr<const base_algorithm> const_algorithm_handle;
+#endif
+
+#ifdef VIENNAMESH_USES_CPP11
+  typedef std::shared_ptr<base_algorithm> algorithm_handle;
+  typedef std::shared_ptr<const base_algorithm> const_algorithm_handle;
+#endif
 
 
   template<typename ValueT>
@@ -129,43 +136,61 @@ namespace viennamesh
 
 
     // sets an input parameter
-    void set_input( string const & name, const_parameter_handle const & parameter )
-    { inputs.set(name, parameter); }
-
-    void set_input( string const & name, parameter_handle const & parameter )
-    { inputs.set(name, parameter); }
-
     template<typename TypeT>
-    void set_input( string const & name, TypeT const & value )
+    void generic_set_input( std::string const & name, TypeT const & value )
     { inputs.set( name, static_cast<parameter_handle const &>(make_parameter(value)) ); }
 
     template<typename ValueT>
-    void set_input( string const & name, parameter_handle_t< const parameter_wrapper<ValueT> > const & parameter )
-    { inputs.set(name, static_cast<const_parameter_handle const &>(parameter)); }
+    void generic_set_input( std::string const & name, parameter_handle_t< parameter_wrapper<ValueT> > const & parameter )
+    { inputs.set(name, static_cast<parameter_handle const &>(parameter)); }
+
+    void set_input( std::string const & name, const_parameter_handle const & parameter )
+    { inputs.set(name, parameter); }
+
+    void set_input( std::string const & name, parameter_handle const & parameter )
+    { inputs.set(name, parameter); }
+
+    template<typename TypeT>
+    void set_input( std::string const & name, TypeT const & value )
+    { generic_set_input(name, value); }
 
     template<typename ValueT>
-    void set_input( string const & name, parameter_handle_t< parameter_wrapper<ValueT> > const & parameter )
+    void set_input( std::string const & name, parameter_handle_t< const parameter_wrapper<ValueT> > const & parameter )
+    { generic_set_input(name, parameter); }
+
+    template<typename ValueT>
+    void set_input( std::string const & name, parameter_handle_t< parameter_wrapper<ValueT> > const & parameter )
     { inputs.set(name, static_cast<parameter_handle const &>(parameter)); }
 
-    void set_input( string const & name, parameter_handle_t<parameter_link> const & parameter )
+    void set_input( std::string const & name, parameter_handle_t<parameter_link> const & parameter )
     { inputs.set(name, static_cast<parameter_handle const &>(parameter)); }
 
-    void set_input( string const & name, char const * value )
+    void set_input( std::string const & name, char const * value )
     { set_input( name, make_parameter( string(value) ) ); }
 
-    void set_input( string const & name, char * value )
+    void set_input( std::string const & name, char * value )
     { set_input( name, make_parameter( string(value) ) ); }
 
-    void link_input( string const & name, algorithm_handle const & source_algorithm, string const & source_name )
+    void set_input( std::string const & name, bool value )
+    { generic_set_input(name, value); }
+
+    void set_input( std::string const & name, int value )
+    { generic_set_input(name, value); }
+
+    void set_input( std::string const & name, double value )
+    { generic_set_input(name, value); }
+
+
+    void link_input( std::string const & name, algorithm_handle const & source_algorithm, string const & source_name )
     { set_input( name, make_parameter_link(source_algorithm, source_name) ); }
 
 
     // unsets an input parameter
-    void unset_input( string const & name ) { inputs.unset(name); }
+    void unset_input( std::string const & name ) { inputs.unset(name); }
 
 
     // queries an input parameter
-    const_parameter_handle get_input( string const & name ) const
+    const_parameter_handle get_input( std::string const & name ) const
     {
       const_parameter_handle parameter = inputs.get(name);
 
@@ -180,7 +205,7 @@ namespace viennamesh
 
   protected:
 
-    const_parameter_handle get_required_input( string const & name ) const
+    const_parameter_handle get_required_input( std::string const & name ) const
     {
       const_parameter_handle param = get_input(name);
       if (!param)
@@ -192,7 +217,7 @@ namespace viennamesh
 
     // queries an input parameter of special type
     template<typename ValueT>
-    typename result_of::const_parameter_handle<ValueT>::type get_input( string const & name ) const
+    typename result_of::const_parameter_handle<ValueT>::type get_input( std::string const & name ) const
     {
       const_parameter_handle parameter = get_input(name);
       if (!parameter) return typename result_of::const_parameter_handle<ValueT>::type();
@@ -210,7 +235,7 @@ namespace viennamesh
 
     // queries an input parameter of special type, throws input_parameter_not_found_exception if not found
     template<typename ValueT>
-    typename result_of::const_parameter_handle<ValueT>::type get_required_input( string const & name ) const
+    typename result_of::const_parameter_handle<ValueT>::type get_required_input( std::string const & name ) const
     {
       typename result_of::const_parameter_handle<ValueT>::type parameter = get_input<ValueT>(name);
       if (!parameter)
@@ -222,27 +247,27 @@ namespace viennamesh
 
     // copies input parameter of special type to value, only works if input is present and convertable, returns true if copy was performed
     template<typename ValueT>
-    bool copy_input( string const & name, ValueT & value ) const
+    bool copy_input( std::string const & name, ValueT & value ) const
     { return inputs.copy_if_present(name, value); }
 
   public:
 
     // references an output parameter to a specific value, doesn't take ownership, remembers pointer to value
     template<typename TypeT>
-    void set_output( string const & name, TypeT & value )
+    void set_output( std::string const & name, TypeT & value )
     { outputs.set( name, make_reference_parameter(value) ); }
 
-    void set_output( string const & name, parameter_handle const & handle )
+    void set_output( std::string const & name, parameter_handle const & handle )
     { outputs.set( name, parameter_handle( new parameter_handle_reference(handle) ) ); }
 
-    void set_output( string const & name, parameter_handle & handle )
+    void set_output( std::string const & name, parameter_handle & handle )
     { outputs.set( name, parameter_handle( new parameter_handle_reference(handle) ) ); }
 
   protected:
 
     // gets a proxy for an output parameter, only way of setting an output parameter, used within an algorithm
     template<typename ValueT>
-    output_parameter_proxy<ValueT> output_proxy( string const & name )
+    output_parameter_proxy<ValueT> output_proxy( std::string const & name )
     {
       output_parameter_proxy<ValueT> proxy;
       proxy.init(outputs, name);
@@ -252,19 +277,19 @@ namespace viennamesh
   public:
 
     // queries an output parameter
-    const_parameter_handle get_output( string const & name ) const
+    const_parameter_handle get_output( std::string const & name ) const
     { return outputs.get(name); }
 
-    parameter_handle get_output( string const & name )
+    parameter_handle get_output( std::string const & name )
     { return outputs.get(name); }
 
     // queries an output parameter of special type
     template<typename ValueT>
-    typename result_of::const_parameter_handle<ValueT>::type get_output( string const & name ) const
+    typename result_of::const_parameter_handle<ValueT>::type get_output( std::string const & name ) const
     { return outputs.get<ValueT>(name); }
 
     template<typename ValueT>
-    typename result_of::parameter_handle<ValueT>::type get_output( string const & name )
+    typename result_of::parameter_handle<ValueT>::type get_output( std::string const & name )
     { return outputs.get<ValueT>(name); }
 
     // unsets an output parameter
