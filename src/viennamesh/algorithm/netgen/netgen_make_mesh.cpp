cd6fed2cb1960e547f473f292398b4275c13d3b5
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-05-06 15:33:39 +0200

    added netgen to algorithm factory and all.h, code cleanup

--- src/viennamesh/algorithm/netgen/netgen_make_mesh.cpp
+++ src/viennamesh/algorithm/netgen/netgen_make_mesh.cpp
@@ -9,12 +9,7 @@ namespace viennamesh
   {
     make_mesh::make_mesh() :
       input_mesh(*this, "mesh"),
-//       relative_find_identic_surface_eps(*this, "relative_find_identic_surface_eps", 1e-8),
       cell_size(*this, "cell_size", 1e10),
-//       grading(*this, "grading", 0.3),
-//       optimization_steps(*this, "optimization_steps", 3),
-//       delaunay(*this, "delaunay", true),
-//       optimize_string(*this, "optimize_string"),
       output_mesh(*this, "mesh") {}
 
     string make_mesh::name() const { return "Netgen 5.1 CSG mesher"; }
@@ -39,8 +34,6 @@ namespace viennamesh
         MeshVolume (mesh_parameters, omp()(i));
         RemoveIllegalElements (omp()(i));
         OptimizeVolume (mesh_parameters, omp()(i));
-
-//         nglib::Ng_Result result = nglib::Ng_GenerateVolumeMesh(it->second, &mesh_parameters);
       }
 
       std_capture().finish();
