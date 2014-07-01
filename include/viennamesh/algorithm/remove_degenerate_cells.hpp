6e0b493910adc2e98f30f830cc951cd4bc095efa
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-07-01 16:54:53 +0200

    added remove degenerate cells algorithm

---
+++ include/viennamesh/algorithm/remove_degenerate_cells.hpp
@@ -0,0 +1,41 @@
+#ifndef VIENNAMESH_ALGORITHM_VIENNAGRID_REMOVE_DEGENERATE_CELLS_HPP
+#define VIENNAMESH_ALGORITHM_VIENNAGRID_REMOVE_DEGENERATE_CELLS_HPP
+
+/* ============================================================================
+   Copyright (c) 2011-2014, Institute for Microelectronics,
+                            Institute for Analysis and Scientific Computing,
+                            TU Wien.
+
+                            -----------------
+                ViennaMesh - The Vienna Meshing Framework
+                            -----------------
+
+                    http://viennamesh.sourceforge.net/
+
+   License:         MIT (X11), see file LICENSE in the base directory
+=============================================================================== */
+
+#include "viennamesh/core/algorithm.hpp"
+
+namespace viennamesh
+{
+  class remove_degenerate_cells : public base_algorithm
+  {
+  public:
+    remove_degenerate_cells();
+
+    std::string name() const;
+    std::string id() const;
+
+    template<typename MeshT, typename SegmentationT>
+    bool generic_run();
+    bool run_impl();
+
+  private:
+    dynamic_required_input_parameter_interface                                  input_mesh;
+
+    output_parameter_interface                                                  output_mesh;
+  };
+}
+
+#endif
