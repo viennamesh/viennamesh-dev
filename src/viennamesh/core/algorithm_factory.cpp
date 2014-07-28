768464f557ef6a8a3c0cb31ca55383f913e38333
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-07-28 18:36:22 +0200

    merge duplicate points algorithm

--- src/viennamesh/core/algorithm_factory.cpp
+++ src/viennamesh/core/algorithm_factory.cpp
@@ -57,6 +57,7 @@ namespace viennamesh
     register_algorithm<viennamesh::seed_point_segmenting>();
     register_algorithm<viennamesh::remove_degenerate_cells>();
     register_algorithm<viennamesh::remove_duplicate_cells>();
+    register_algorithm<viennamesh::merge_duplicate_points>();
 
     register_algorithm<viennamesh::map_segments>();
     register_algorithm<viennamesh::merge_meshes>();
