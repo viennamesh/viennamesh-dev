87f67fe652c862d9203ea45751b3b978fdc18206
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-02-17 16:13:03 +0100

    support for hull laplacian smoothing

--- plugins/viennagrid/laplace_smooth.cpp
+++ plugins/viennagrid/laplace_smooth.cpp
@@ -15,12 +15,13 @@
 #include "laplace_smooth.hpp"
 #include "viennagrid/core.hpp"
 
+using namespace std::placeholders;
+
 namespace viennamesh
 {
   // http://en.wikipedia.org/wiki/Laplacian_smoothing
   // http://graphics.stanford.edu/courses/cs468-12-spring/LectureSlides/06_smoothing.pdf
-  template<typename NumericT>
-  void laplace_smooth_impl( viennagrid::mesh_t const & mesh, NumericT lambda )
+  void laplace_smooth_impl( viennagrid::mesh_t const & mesh, viennagrid_numeric lambda )
   {
     typedef viennagrid::mesh_t MeshType;
 
@@ -70,6 +71,100 @@ namespace viennamesh
   }
 
 
+
+
+
+
+
+
+  template<typename RegionRangeT>
+  bool is_subset(RegionRangeT const & subset, RegionRangeT const & superset)
+  {
+    typedef typename viennagrid::result_of::const_iterator<RegionRangeT>::type RegionRangeIterator;
+
+    for (RegionRangeIterator it = subset.begin(); it != subset.end(); ++it)
+    {
+      RegionRangeIterator jt = superset.begin();
+      for (; jt != superset.end(); ++jt)
+        if ( (*it).id() == (*jt).id() )
+          break;
+
+      if (jt == superset.end())
+        return false;
+    }
+
+    return true;
+  }
+
+
+  void hull_laplace_smooth_impl( viennagrid::mesh_t const & mesh, viennagrid_numeric lambda )
+  {
+    typedef viennagrid::mesh_t MeshType;
+
+    typedef typename viennagrid::result_of::point<MeshType>::type PointType;
+    typedef typename viennagrid::result_of::element<MeshType>::type VertexType;
+
+    typedef typename viennagrid::result_of::vertex_range<MeshType>::type VertexRangeType;
+    typedef typename viennagrid::result_of::iterator<VertexRangeType>::type VertexIteratorType;
+
+    viennagrid_dimension dim = viennagrid::geometric_dimension(mesh);
+
+    VertexRangeType vertices(mesh);
+
+    std::vector<PointType> vertex_offset_container(vertices.size(), PointType(dim));
+    typename viennagrid::result_of::accessor<std::vector<PointType>, VertexType>::type vertex_offset(vertex_offset_container);
+
+    for (VertexIteratorType vit = vertices.begin(); vit != vertices.end(); ++vit)
+    {
+      typedef typename viennagrid::result_of::region_range<MeshType, VertexType>::type RegionRangeType;
+      typedef typename viennagrid::result_of::iterator<RegionRangeType>::type RegionRangeIterator;
+
+      typedef typename viennagrid::result_of::neighbor_range<MeshType>::type NeighborVertexRangeType;
+      typedef typename viennagrid::result_of::iterator<NeighborVertexRangeType>::type NeighborVertexRangeIterator;
+
+      PointType offset(dim);
+      int point_count = 0;
+
+      PointType point = viennagrid::get_point(*vit);
+
+      NeighborVertexRangeType neighbor_vertices(mesh, *vit, 1, 0);
+
+      RegionRangeType regions(mesh, *vit);
+//       std::cout << regions.size() << std::endl;
+
+      if (regions.size() == 2)
+      {
+        for (NeighborVertexRangeIterator nvit = neighbor_vertices.begin(); nvit != neighbor_vertices.end(); ++nvit)
+        {
+          offset += viennagrid::get_point(*nvit) - point;
+          ++point_count;
+        }
+      }
+//       else if (regions.size() == 3)
+//       {
+//         for (NeighborVertexRangeIterator nvit = neighbor_vertices.begin(); nvit != neighbor_vertices.end(); ++nvit)
+//         {
+//           RegionRangeType neighbor_regions(mesh, *nvit);
+//           if ( is_subset(neighbor_regions, regions) )
+//           {
+//             offset += viennagrid::get_point(*nvit) - point;
+//             ++point_count;
+//           }
+//         }
+//       }
+
+      if (point_count != 0)
+        offset /= point_count;
+      vertex_offset(*vit) = offset * lambda;
+    }
+
+    for (VertexIteratorType vit = vertices.begin(); vit != vertices.end(); ++vit)
+      viennagrid::set_point( *vit, viennagrid::get_point(*vit) + vertex_offset(*vit) );
+  }
+
+
+
+
   laplace_smooth::laplace_smooth() {}
   std::string laplace_smooth::name() { return "laplace_smooth"; }
 
@@ -83,6 +178,9 @@ namespace viennamesh
     if (!input_mesh)
       return false;
 
+    int geometric_dimension = viennagrid::geometric_dimension( input_mesh() );
+    int cell_dimension = viennagrid::cell_dimension( input_mesh() );
+
     if (!lambda)
       return false;
 
@@ -94,8 +192,28 @@ namespace viennamesh
     if (output_mesh != input_mesh)
       viennagrid::copy( input_mesh(), output_mesh() );
 
+
+    std::function< void(viennagrid::mesh_t const &) > smooth_function;
+    if (geometric_dimension == 3 && cell_dimension == 2)
+    {
+      info(1) << "Geometric dimension == 3 and cell dimension == 2 -> using hull laplacian smoothing" << std::endl;
+      smooth_function = std::bind( hull_laplace_smooth_impl, _1, lambda() );
+    }
+    else if (geometric_dimension == cell_dimension)
+    {
+      info(1) << "Geometric dimension == cell dimension -> using standard laplacian smoothing" << std::endl;
+      smooth_function = std::bind( laplace_smooth_impl, _1, lambda() );
+    }
+    else
+    {
+      info(1) << "Laplacian smoothing not supported for geometric dimension == " << geometric_dimension << " and cell dimension == " << cell_dimension << std::endl;
+      return false;
+    }
+
+//     typedef void (*hull_laplace_smooth_impl)(viennagrid::mesh_t const &, double) SmoothFunction
+
     for (int i = 0; i < iteration_count(); ++i)
-      laplace_smooth_impl(output_mesh(), lambda());
+      smooth_function( output_mesh() );
 
     set_output( "mesh", output_mesh );
 
