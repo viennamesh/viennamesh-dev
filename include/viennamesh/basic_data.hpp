380462d218ed8a4e722c3b396d4f456073b69aae
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-03-23 12:29:16 +0100

    checkin before delete

--- include/viennamesh/basic_data.hpp
+++ include/viennamesh/basic_data.hpp
@@ -1,93 +1,41 @@
 #ifndef _VIENNAMESH_BASIC_DATA_HPP_
 #define _VIENNAMESH_BASIC_DATA_HPP_
 
-#include "viennamesh/forwards.hpp"
-#include "viennagrid/quantity_field.hpp"
-#include <string>
+// #include "viennamesh/forwards.hpp"
+// #include "viennagrid/quantity_field.hpp"
+// #include <string>
+// #include "viennamesh/backend/backend_common.hpp"
 
-typedef struct viennamesh_string_t * viennamesh_string;
-typedef struct viennamesh_point_container_t * viennamesh_point_container;
-typedef struct viennamesh_seed_point_container_t * viennamesh_seed_point_container;
-typedef struct viennagrid_mesh_ * viennagrid_mesh;
+// typedef struct viennamesh_string_t * viennamesh_string;
+// typedef struct viennamesh_point_container_t * viennamesh_point_container;
+// typedef struct viennamesh_seed_point_container_t * viennamesh_seed_point_container;
+// typedef struct viennagrid_mesh_ * viennagrid_mesh;
 
 
 namespace viennamesh
 {
-  typedef std::vector<point_t> PointContainerType;
-  typedef std::vector< std::pair<point_t, int> > SeedPointContainerType;
-
-  void convert( PointContainerType const & points, viennamesh_point_container internal_points );
-  void convert( viennamesh_point_container internal_points, PointContainerType & points );
-
-  void convert( point_t const & point, viennamesh_point_container internal_points );
-  void convert( viennamesh_point_container internal_points, point_t & point );
-
-  void convert( SeedPointContainerType const & seed_points, viennamesh_seed_point_container internal_seed_points );
-  void convert( viennamesh_seed_point_container internal_seed_points, SeedPointContainerType & seed_points );
 
 
 
-  namespace result_of
-  {
-    template<typename T>
-    struct data_information;
+//   std::pair<point_t, std::string> SeedPointType;
 
-    template<>
-    struct data_information<bool>
-    {
-      static std::string type_name() { return "bool"; }
-      static std::string local_binary_format() { return ""; }
-    };
+//   void convert( viennagrid::point_t );
 
-    template<>
-    struct data_information<int>
-    {
-      static std::string type_name() { return "int"; }
-      static std::string local_binary_format() { return ""; }
-    };
+//   void convert( PointContainerType const & points, viennamesh_point_container internal_points );
+//   void convert( viennamesh_point_container internal_points, PointContainerType & points );
 
-    template<>
-    struct data_information<double>
-    {
-      static std::string type_name() { return "double"; }
-      static std::string local_binary_format() { return ""; }
-    };
+//   void convert( viennagrid::point_t const & point, viennamesh_point internal_point );
+//   void convert( viennamesh_point internal_point, viennagrid::point_t & point );
+//
+//   void convert( seed_point_t const & seed_point, viennamesh_seed_point internal_seed_point );
+//   void convert( viennamesh_seed_point internal_seed_point, seed_point_t & seed_point );
 
-    template<>
-    struct data_information<viennamesh_string>
-    {
-      static std::string type_name() { return "viennamesh_string"; }
-      static std::string local_binary_format() { return ""; }
-    };
+//   void convert( SeedPointContainerType const & seed_points, viennamesh_seed_point_container internal_seed_points );
+//   void convert( viennamesh_seed_point_container internal_seed_points, SeedPointContainerType & seed_points );
 
-    template<>
-    struct data_information<viennamesh_point_container>
-    {
-      static std::string type_name() { return "viennamesh_point_container"; }
-      static std::string local_binary_format() { return ""; }
-    };
 
-    template<>
-    struct data_information<viennamesh_seed_point_container>
-    {
-      static std::string type_name() { return "viennamesh_seed_point_container"; }
-      static std::string local_binary_format() { return ""; }
-    };
 
-    template<>
-    struct data_information<viennagrid_quantity_field>
-    {
-      static std::string type_name() { return "viennagrid_quantity_field"; }
-      static std::string local_binary_format() { return ""; }
-    };
 
-    template<>
-    struct data_information<viennagrid_mesh>
-    {
-      static std::string type_name() { return "viennagrid_mesh"; }
-      static std::string local_binary_format() { return ""; }
-    };
-  }
 }
 
 #endif
