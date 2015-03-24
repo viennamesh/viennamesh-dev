a5ed1271f840a9f7ba1051a9ed633fe2ff303ae1
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-03-24 11:28:12 +0100

    clean-up

--- plugins/viennamesh_plugin.cpp
+++ plugins/viennamesh_plugin.cpp
@@ -5,7 +5,8 @@ int viennamesh_version()
   return VIENNAMESH_VERSION;
 }
 
-int viennamesh_plugin_init(viennamesh_context ctx_)
+
+viennamesh_error viennamesh_plugin_init(viennamesh_context ctx_)
 {
   viennamesh::context_handle context(ctx_);
   viennamesh::plugin_init(context);
