142e32d5ea482275e6dbec3f0a3a497e49519298
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-04-30 10:55:58 +0200

    added boost includes/using

--- include/viennameshpp/forwards.hpp
+++ include/viennameshpp/forwards.hpp
@@ -8,10 +8,27 @@
 #include <string>
 
 #include <boost/lexical_cast.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/optional.hpp>
 
 namespace viennamesh
 {
   using boost::lexical_cast;
+
+  using boost::shared_ptr;
+  using boost::make_shared;
+
+  using boost::function;
+  using boost::bind;
+  using ::_1;
+  using ::_2;
+
+  using boost::optional;
+
+
 
   class context_handle;
 
