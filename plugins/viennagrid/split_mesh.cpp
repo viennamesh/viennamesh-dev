c702fcc70853308f8e49febb62bc3862e9961e12
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-08-27 12:44:48 +0200

    added second mode (commented out)

--- plugins/viennagrid/split_mesh.cpp
+++ plugins/viennagrid/split_mesh.cpp
@@ -51,4 +51,35 @@ namespace viennamesh
     return true;
   }
 
+
+
+
+//     RegionRangeType regions( input_mesh() );
+//     if (regions.size() <= 1)
+//     {
+//       mesh_handle output_mesh = make_data<mesh_handle>();
+//       viennagrid::copy( input_mesh(), output_mesh() );
+//       set_output( "mesh", output_mesh );
+//     }
+//     else
+//     {
+//       mesh_handle output_meshes = make_data<mesh_handle>();
+//       output_meshes.resize( regions.size() );
+//
+//       int region_index = 0;
+//       for (RegionRangeIterator rit = regions.begin(); rit != regions.end(); ++rit, ++region_index)
+//       {
+//         viennagrid::mesh mesh = output_meshes(region_index);
+//
+//         viennagrid::result_of::element_copy_map<>::type copy_map( mesh, false );
+//         viennagrid::copy(copy_map, *rit, mesh, viennagrid::true_functor());
+//       }
+//
+//       set_output( "mesh", output_meshes );
+//
+//       info(1) << "Split mesh into " << region_index << " meshes" << std::endl;
+//     }
+
+
+
 }
