243af226e8e7fea1a2b42d1a6211ed2b25dde3c7
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2016-05-23 16:41:45 +0200

    added plc_writer algorithm

--- plugins/io/plugin.cpp
+++ plugins/io/plugin.cpp
@@ -2,6 +2,7 @@
 #include "mesh_reader.hpp"
 #include "mesh_writer.hpp"
 #include "plc_reader.hpp"
+#include "plc_writer.hpp"
 
 
 viennamesh_error viennamesh_plugin_init(viennamesh_context context)
@@ -10,6 +11,7 @@ viennamesh_error viennamesh_plugin_init(viennamesh_context context)
   viennamesh::register_algorithm<viennamesh::mesh_writer>(context);
 
   viennamesh::register_algorithm<viennamesh::plc_reader>(context);
+  viennamesh::register_algorithm<viennamesh::plc_writer>(context);
 
   return VIENNAMESH_SUCCESS;
 }
